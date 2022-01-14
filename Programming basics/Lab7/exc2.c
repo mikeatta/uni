74956f496e1dc0ea11c0060c59ee187fee7cd842
mike <saintsikorski@gmail.com>
2022-01-14 14:23:50 +0100

    Added loop to receive user input.

--- Programming basics/Lab7/exc2.c
+++ Programming basics/Lab7/exc2.c
@@ -4,7 +4,9 @@
 int contains(char character, char *arr)
 {
     int i, char_present = 0;
-    for(i=0; i<strlen(arr); i++) // Check entire string for presence of charcter, if present set variable to 1
+
+    /* check entire string for presence of charcter, if present set variable to 1 */
+    for(i=0; i<strlen(arr); i++)
     {
         if(character == arr[i])
             char_present = 1;
@@ -12,10 +14,19 @@ int contains(char character, char *arr)
             char_present;
     }
 
-    if(char_present == 1) // If character was found, return true (1)
+    /* if character was found, return true (1) */
+    if(char_present == 1)
         return 1;
     else
         return 0;
+}
+
+/* function to clear '\n' char from user input */
+void clear_stdin(void)
+{
+    int c = getchar();
+    while (c!='\n' && c!=EOF)
+        c = getchar();
 }
 
 int main()
@@ -23,19 +34,33 @@ int main()
     char string[100];
     char input;
     char *ptr = string;
+    int i, y;
+    int char_present = 0;
     
     puts("Enter a string:");
     fgets(string, 100, stdin);
-    puts("Enter a character to check for:");
-    input = getchar();
+    string[strcspn(string, "\n")] = 0; // Removing '\n' char from string
 
-    // puts("\n--------------");
-    // fputs(string, stdout);
-    // printf("%c", input);
+    do
+    {
+        int single_char = 0;
+        puts("Enter a charactes to check for:");
+        for(y=0; y<=(strlen(string))-1; y++)
+        {
+            input = getchar(); // Getting the character from user
+            int result = contains(input, ptr);
+            clear_stdin(); // Clearing input
+            if(result==1)
+            {
+                single_char = 1;
+                char_present = single_char;
+            }
+            else
+                single_char = 0;
+        }
+        i = y;
+    } while(i<=(strlen(string))-1);
 
-    // USE THE 'CONTAINS' FUNCTION IN A LOOP
-
-    int result = contains(input, ptr);
-    printf("Result: %d", result);
+    printf("Result: %d", char_present);
     return 0;
 }
