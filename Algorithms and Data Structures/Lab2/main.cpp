3b99d7080f95c148f3d3529f3ca34a07e1d97831
mike <saintsikorski@gmail.com>
2022-03-13 23:33:24 +0100

    Implemented power function

--- Algorithms and Data Structures/Lab2/main.cpp
+++ Algorithms and Data Structures/Lab2/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 void hello_world() {
+
     std::cout << "Hello world!\n";
 }
 
 int greatest_common_divisor(int first_number, int second_number) {
+
     int gcd, division_remainder;
 
     if(first_number == 0) {
@@ -21,6 +23,7 @@ int greatest_common_divisor(int first_number, int second_number) {
 }
 
 int least_common_multiple(int first_number, int second_number) {
+
     int lcm;
 
     lcm = (first_number * second_number) / greatest_common_divisor(first_number, second_number);
@@ -28,6 +31,7 @@ int least_common_multiple(int first_number, int second_number) {
 }
 
 int factorial_iteration(int number) {
+
     int factorial = 1;
     for(int i=number; i>0; i--) {
         factorial = factorial * i;
@@ -36,8 +40,22 @@ int factorial_iteration(int number) {
 }
 
 int factorial_recursion(int number) {
+    
     if(number <= 1) return 1;
     else return number * factorial_recursion(number - 1);
+}
+
+int power_of_number(int base, int exponent) {
+
+    int i;
+    int power = 0;
+    // std::cout << "Enter power base: ";
+    // std::cout << "Enter power exponent: ";
+
+    for(i=0; i<exponent; i++) {
+        power += base * base;
+    }
+    return power;
 }
 
 int main() {
@@ -52,10 +70,12 @@ int main() {
     std::cout << "GCM: " << greatest_common_divisor(270, 192);
     std::cout << "\n";
 
-
     // // Exc 3 - Factorials
     std::cout << "Factorial by iteration: " << factorial_iteration(4) << "\n";
     std::cout << "Factorial by recursion: " << factorial_recursion(7) << "\n";
+
+    // Exc 4 - Power of a
+    std::cout << "Power equals to: " << power_of_number(5, 5) << "\n";
 
     return 0;
 }
