52dcce4ce8d94a7bec456626eb50a2a51914356d
mike <saintsikorski@gmail.com>
2022-03-25 22:14:44 +0100

    Added decimal to binary conversion

---
+++ Algorithms and Data Structures/Lab4/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() 
+{
+    int i;
+
+    // Exc 2
+    int dec_tab[99];
+    int decimal_number_to_convert;
+
+    std::cout << "Decimal number (to bin): ";
+    std::cin >> decimal_number_to_convert;
+
+    for(i=0; decimal_number_to_convert>0; i++)
+    {
+        dec_tab[i] = decimal_number_to_convert % 2;
+        decimal_number_to_convert = decimal_number_to_convert / 2;
+    }
+
+    std::cout << "Binary: ";
+
+    for(i=i-1; i>=0; i--)
+    {
+        std::cout << dec_tab[i];
+    }
+
+    std::cout << "\n";
+
+    return 0;
+}
