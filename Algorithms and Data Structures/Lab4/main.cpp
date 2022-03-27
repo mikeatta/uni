473b361c93036628d2435996b7dbf2873f6404b3
mike <saintsikorski@gmail.com>
2022-03-27 17:28:54 +0200

    Implemented decimal to hexadecimal conversion

--- Algorithms and Data Structures/Lab4/main.cpp
+++ Algorithms and Data Structures/Lab4/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <math.h> // For pow function [Exc2]
+#include <string.h>
 
 int main() 
 {
     int i;
 
     // Exc 2
+
     // Dec -> bin
     int dec_tab[99];
     int decimal_number_to_convert;
@@ -45,6 +47,47 @@ int main()
     }
 
     std::cout << "Binary: " << dec << "\n";
+
+    // Exc 3
+
+    // Dec -> Hex
+    std::string hex_tab[99];
+    decimal_number_to_convert = 0;
+    rem = 0;
+
+    std::cout << "Decimal number (to hex): ";
+    std::cin >> decimal_number_to_convert;
+    dec = decimal_number_to_convert;
+
+    for(i=0; dec>0; i++)
+    {
+        rem = dec % 16;
+        if(rem>9)
+        {
+            switch (rem)
+            {
+                case 10: hex_tab[i] = "A"; break;
+                case 11: hex_tab[i] = "B"; break;
+                case 12: hex_tab[i] = "C"; break;
+                case 13: hex_tab[i] = "D"; break;
+                case 14: hex_tab[i] = "E"; break;
+                case 15: hex_tab[i] = "F"; break;
+            }
+        }
+        else
+        {
+            std::string s = std::to_string(rem);
+            hex_tab[i] = s;
+        }
+        dec = dec / 16;
+    }
+    std::cout << "Hex: ";
+
+    for(i-1; i>=0; i--)
+    {
+        std::cout << hex_tab[i];   
+    }
+    std::cout << "\n";
 
     return 0;
 }
