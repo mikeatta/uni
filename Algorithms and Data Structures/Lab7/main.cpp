b6c1b0ecaecbb4824d7e399b29ce1981057fc0c8
mike <saintsikorski@gmail.com>
2022-04-20 12:35:53 +0200

    Implemented merge sort function

--- Algorithms and Data Structures/Lab7/main.cpp
+++ Algorithms and Data Structures/Lab7/main.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
 
-void merge(int arr[], int left, int middle, int right);
+void merge(int arr[], int left, int middle, int right)
+{
+    int i = left; // staring index of left subarray
+    int j = middle + 1; // starting index of right subarray
+    int k = left; // starting index of temp array
+
+    int temp[10];
+
+    while(i<=middle && j<=right)
+    {
+        if(arr[i]<=arr[j])
+        {
+            temp[k] = arr[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            temp[k] = arr[j];
+            j++;
+            k++;
+        }
+    }
+    while(i<=middle) // copying all elements from left arr to temp
+    {
+        temp[k] = arr[i];
+        i++;
+        k++;
+    }
+    while(j<=right) // copying all elements from right arr to temp
+    {
+        temp[k] = arr[j];
+        j++;
+        k++;
+    }
+    for(int s = left; s <= right; s++)
+    {
+        arr[s] = temp[s]; // copying elements from temp array
+    }
+}
 
 void merge_sort(int arr[], int left, int right)
 {
@@ -22,6 +61,7 @@ int main()
     for(int i = 0; i < 10; i++)
     {
         std::cout << arr[i] << " ";
+        if(i == 9) std::cout << std::endl;
     }
 
     // Sorting the array
