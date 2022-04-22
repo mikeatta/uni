477481d822c4b2c0d39aa9fe4bb065574fa0a340
mike <saintsikorski@gmail.com>
2022-04-22 16:31:23 +0200

    Started new test project

---
+++ Development tools/SearchAlgorithms.Tests/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SearchAlgorithms.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            int varOne = 50;
+            int varTwo = 50;
+            int expResult = 100;
+            
+            Assert.IsTrue(varOne + varTwo == expResult, "Should be true.");
+            Assert.IsTrue(varOne + varTwo == expResult+1, "Should be false.");
+        }
+    }
+}
