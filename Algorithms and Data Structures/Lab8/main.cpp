ba1ae5eecc964795aad7615a3d383c1f55bdfca5
mike <saintsikorski@gmail.com>
2022-04-27 10:38:43 +0200

    Writing and reading from test file

--- Algorithms and Data Structures/Lab8/main.cpp
+++ Algorithms and Data Structures/Lab8/main.cpp
@@ -1,14 +1,26 @@
+#include <iostream>
 #include <fstream>
 
 int main()
 {
-    std::ofstream file;
-    file.open("..\\test_file.txt");
+    // Writing to file
+    std::ofstream file_write;
+    file_write.open("..\\test_file.txt");
     std::string line = "Line num: ";
     for(int i = 0; i < 5; i++)
     {
-        file << line << i << "\n";
+        file_write << line << i << "\n";
     }
-    file.close();
+    file_write.close();
+
+    // Reading from file
+    std::fstream file_read;
+    file_read.open("..\\test_file.txt");
+//    std::string line;
+    while(file_read.good())
+    {
+        getline(file_read, line);
+        std::cout << line << "\n";
+    }
     return 0;
 }
