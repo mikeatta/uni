f1c87df1d2559694f6121e56f0983bd20c7191d3
mike <saintsikorski@gmail.com>
2022-04-28 17:25:05 +0200

    Generating random number file sets

--- Algorithms and Data Structures/Lab8/main.cpp
+++ Algorithms and Data Structures/Lab8/main.cpp
@@ -1,26 +1,48 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+int number_gen()
+{
+    return rand() % (10000+1-0) + 0;
+}
+
+void make_file(int amount_of_files)
+{
+
+    int i = 0, j = 0;
+    int set_number_amount;
+    while(i<amount_of_files) // How many files to make
+    {
+        std::cout << "How many sets of numbers for set " << i+1 << "?" <<std::endl;
+        std::cin >> set_number_amount;
+
+
+        std::string set_name;
+        set_name = std::to_string(set_number_amount);
+        set_name = "..\\" + set_name + ".txt";
+
+        std::ofstream write_file; // Creating files
+        write_file.open(set_name);
+        for(;j<set_number_amount;j++)
+        {
+            write_file << number_gen() << "\n";
+        }
+        write_file.close();
+        i++;
+    }
+}
 
 int main()
 {
-    // Writing to file
-    std::ofstream file_write;
-    file_write.open("..\\test_file.txt");
-    std::string line = "Line num: ";
-    for(int i = 0; i < 5; i++)
-    {
-        file_write << line << i << "\n";
-    }
-    file_write.close();
+    int file_count;
+    srand(time(0));
 
-    // Reading from file
-    std::fstream file_read;
-    file_read.open("..\\test_file.txt");
-//    std::string line;
-    while(file_read.good())
-    {
-        getline(file_read, line);
-        std::cout << line << "\n";
-    }
+    // Writing to file
+    std::cout << "How many files to make?\n";
+    std::cin >> file_count;
+    make_file(file_count);
     return 0;
 }
