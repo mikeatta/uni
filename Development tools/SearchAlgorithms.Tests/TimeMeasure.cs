1c55ba5968b601f8eae7fb45e3323c07b6b09636
imparpaing <saintsikorski@gmail.com>
2022-05-10 14:59:24 +0200

    Generating random numbers in a loop

---
+++ Development tools/SearchAlgorithms.Tests/TimeMeasure.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SearchAlgorithms.Tests
+{
+    internal class TimeMeasure
+    {
+        public static void Main(string[] args)
+        {
+            int[] randomNumbers = new int[100000]; // Storing unsorted numbers
+            Random random = new Random(); 
+
+            for (int i = 0; i < 100000; i++) 
+            {
+                int randNum = random.Next(1, 5000); // Creating and assigning random numbers to array
+                randomNumbers[i] = randNum;
+            }
+            
+            for (int i = 0; i < 100000; i++)
+            {
+                Console.Write(randomNumbers[i] + ", ");
+            }
+        }
+    }
+}
