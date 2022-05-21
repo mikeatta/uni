b2b47c567b5a4b5743cfe0a5f05f12e67f17e61d
imparpaing <saintsikorski@gmail.com>
2022-05-21 14:11:35 +0200

    Creating stack CLI

--- Algorithms and Data Structures/Lab12/stack.cpp
+++ Algorithms and Data Structures/Lab12/stack.cpp
@@ -4,25 +4,49 @@
 
 int main()
 {
+
     std::stack<std::string> myStack; // Creating the stack
 
-    std::cout << "Placing items on the stack...\n";
-    myStack.push("this");
-    myStack.push("is");
-    myStack.push("a test");
-    std::cout << "Top stack element is: " << myStack.top() << "\n"; // Adding elements to the stack
+    int store_menu_choice;
+    std::string store_user_input;
+    bool display_menu = true;
 
-    myStack.top() = "New top element";
-    std::cout << "This is the new top element: " << myStack.top() << "\n"; // Replacing stack top element value
+    while(display_menu)
+    {
+        std::cout << "What do you want to do?\n";
+        std::cout << "[ 1] Add element\n[ 2] Remove element\n[ 3] Check if empty\n[ 4] Display amount of elements\n[ 5] Exit\n";
+        std::cin >> store_menu_choice;
 
-    std::cout << "Amount of elements in myStack: " << myStack.size() << "\n"; // Displaying stack size
+        switch (store_menu_choice) {
+            case 1:
+                std::cout << "Place element: ";
+                std::cin >> store_user_input;
+                myStack.push(store_user_input);
+                break;
 
-    myStack.pop();
-    myStack.pop();
-    std::cout << "Top element after popping 2 elements: ['" << myStack.top() << "']\n"; // Taking elements off the stack
-    std::cout << "Amount of elements in myStack: " << myStack.size() << "\n";
+            case 2:
+                std::cout << "Take element off\n";
+                myStack.pop();
+                break;
 
-    myStack.pop();
-    if(myStack.empty()) std::cout << "Stack is empty\n";
+            case 3:
+                std::cout << "Checking if stack is empty...\n";
+                if (myStack.empty()) std::cout << "Stack is empty\n";
+                else std::cout << "Stack is NOT empty\n";
+
+            case 4:
+                std::cout << "There are [" << myStack.size() << "] elements in the stack\n";
+                break;
+
+            case 5:
+                std::cout << "Exiting...\n";
+                display_menu = false;
+                break;
+
+            default:
+                std::cout << "Incorrect input\n";
+                break;
+        }
+    }
     return 0;
 }
