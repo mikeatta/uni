2029bb6fed715ce3a17e5518f1ca60043fd886c0
mike <saintsikorski@gmail.com>
2022-06-01 11:37:58 +0200

    Added CLI menu to vector file

--- Algorithms and Data Structures/Lab12/vector.cpp
+++ Algorithms and Data Structures/Lab12/vector.cpp
@@ -4,30 +4,98 @@
 
 int main()
 {
-    std::vector<std::string> myVector; // Creating a vector
+    std::vector<std::string> myVector; // Creating the vector
 
-    myVector.push_back("element1");
-    myVector.push_back("element2");
-    myVector.push_back("element3"); // Adding elements to the vector
+    int store_menu_choice, lookup_vector_element;
+    std::string store_user_input, lookup_vector_value;
+    bool display_menu = true;
 
-    myVector.insert(myVector.begin() + 2, "element2.5"); // Inserting an element
-    std::cout << "Displaying vector elements: \n";
-    for(int i=0; i<myVector.size(); i++)
-        std::cout << myVector[i] << "\n";
+    // User menu
+    std::cout << "What do you want to do?\n";
+    std::cout <<
+        "[ 1] Add element\n"
+        "[ 2] Remove element\n"
+        "[ 3] Pull nth element\n"
+        "[ 4] Check for element\n"
+        "[ 5] Pull front and back elements\n"
+        "[ 6] Display structure size\n"
+        "[ 7} Check if empty\n"
+        "[ 8] Display menu\n"
+        "[ 9] Exit\n\n";
 
-    myVector.front() = "element0";
-    myVector.back() = "element4";
-    std::cout << "Vector size: " << myVector.size() << "\n"; // Replacing elements at the front and back of vector
+    while(display_menu)
+    {
+        std::cout << "Select: ";
+        std::cin >> store_menu_choice;
 
-    std::cout << "New vector elements: \n";
-    for(int i=0; i<myVector.size(); i++)
-        std::cout << myVector[i] << "\n";
+        switch (store_menu_choice)
+        {
+            case 1: // Add element
+                std::cout << "Placing element: ";
+                std::cin >> store_user_input;
+                myVector.push_back(store_user_input);
+                break;
 
-    myVector.pop_back();
-    myVector.pop_back();
-    myVector.pop_back();
-    myVector.pop_back(); // Removing vector elements
+            case 2: // Remove back element
+                std::cout << "Taking an element off...\n";
+                myVector.pop_back();
+                break;
 
-    if(myVector.empty()) std::cout << "Vector is empty\n";
+            case 3: // Get nth element
+                std::cout << "Which element to pull?\nElement: ";
+                std::cin >> lookup_vector_element;
+                std::cout << "\n";
+                std::cout << lookup_vector_element << "Element at index " << lookup_vector_element << ": " << myVector[lookup_vector_element - 1] << "\n";
+                break;
+
+            case 4: // Check if element exists within the vector
+                bool element_found;
+                element_found = false;
+                std::cout << "Which element to check for?\nElement :";
+                std::cin >> lookup_vector_value;
+                for(int i=0; i<myVector.size(); i++) if(myVector[i] == lookup_vector_value) element_found = true;
+                if(element_found == true) std::cout << "Element present\n";
+                else std::cout << "No element present\n";
+                break;
+
+            case 5: // Display front and back elements
+                std::cout << "Front element: " << myVector.front() << "\n";
+                std::cout << "Back element: " << myVector.back() << "\n";
+                break;
+
+            case 6: // Display size
+                std::cout << "Vector size is: " << myVector.size() << "\n";
+                break;
+
+            case 7: // Check if the vector is empty
+                std::cout << "Checking if vector is empty...\n";
+                if (myVector.empty()) std::cout << "Vector is empty\n";
+                else std::cout << "Vector is NOT empty\n";
+                break;
+
+            case 8: // Display menu
+                std::cout << "What do you want to do?\n";
+                std::cout <<
+                    "[ 1] Add element\n"
+                    "[ 2] Remove element\n"
+                    "[ 3] Check for element\n"
+                    "[ 4] Pull nth element\n"
+                    "[ 5] Pull front and back elements\n"
+                    "[ 6] Display structure size\n"
+                    "[ 7} Check if empty\n"
+                    "[ 8] Display menu\n"
+                    "[ 9] Exit\n\n";
+                break;
+
+            case 9: // Exit
+                std::cout << "Exiting...\n";
+                display_menu = false;
+                break;
+
+            default:
+                std::cout << "Incorrect input\n";
+                break;
+        }
+    }
     return 0;
 }
