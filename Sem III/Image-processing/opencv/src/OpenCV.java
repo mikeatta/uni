d2fafe6a2563704f771c77207b2cd1f861eb03c0
mike <saintsikorski@gmail.com>
2022-10-11 13:56:18 +0200

    Add writing an image

--- Sem III/Image-processing/opencv/src/OpenCV.java
+++ Sem III/Image-processing/opencv/src/OpenCV.java
@@ -1,13 +1,25 @@
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 
 public class OpenCV {
     public static void main(String[] args){
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        Mat img = new Mat(3, 3, CvType.CV_8U);
-        int row = 0, col = 0;
-        img.put(row, col, 1, 2, 3, 4, 5, 6, 7, 8);
-        System.out.println(img.dump());
+
+        // Installing the library
+        Imgcodecs imgcodecs = new Imgcodecs();
+
+        // Image path
+        String imgPath = "/home/zorin/Documents/git/uni/Sem III/Image-processing/img/IMG_2116.jpg";
+        String imgWrite = "/home/zorin/Documents/git/uni/Sem III/Image-processing/processed-imgs/output.jpg";
+
+        // Loading the image
+        Mat matrix = imgcodecs.imread(imgPath);
+        System.out.println("Image successfully loaded");
+
+        // Writing the image
+        imgcodecs.imwrite(imgWrite, matrix);
+        System.out.println("Image successfully written");
     }
 }
