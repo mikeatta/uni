0e6274091fbe44669a6c17d92f9d9f84540a1649
mike <saintsikorski@gmail.com>
2022-10-20 18:15:48 +0200

    Add Lab 3 exercises 2-4

---
+++ Sem III/Script-programming-languages/Lab-3/main.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+
+def main():
+    fillList() # Exc 2 - Fill list with user input 
+    displayUniqueNumbers() # Exc 3 - Display elements present in list1 and not present in list2
+    displayOddNumbers() # Exc 4 - Display only odd numbers, print smallest number
+
+# Exc 2
+def fillList():
+    print('Exc 2 - Fill list with user input')
+    userInputList = [] # Creating the lists
+    oddNumberSum = 0 # Store sum of odd numbers from user input
+    oddNumberCount = 0 # Amount of odd numbers entered
+
+    # Filling list with user input
+    for i in range(10):
+        addListElement = input('Enter el. ' + str(i) + ' : ')
+        userInputList.append(int(addListElement))
+
+    print('')
+
+    # Sorting the list
+    userInputList.sort()
+    print('Smallest element entered: ' + str(userInputList[0])) # Smallest number
+    userInputList.reverse()
+    print('Largest number entered: ' + str(userInputList[0]) + '\n') # Largest number
+
+
+    for i in range(len(userInputList)):
+        if (userInputList[i] % 2 != 0):
+            oddNumberSum =+ i # Add up value of all elements
+            oddNumberCount += 1 # Increment count by one for each odd number [e.g. i++ in Cpp]
+
+    average = oddNumberSum / oddNumberCount # Get element average
+    print('Average of list elements : ' + str(average) + '\n')
+
+# Exc 3
+def displayUniqueNumbers():
+    print('Exc 3 - Display numbers unique to the first list')
+
+    list = [1,2,3,4,5,6,7,8,9,10]
+    list2 = [1,4,4,6,543,-31,0,10,-65,-2]
+    displayList = []
+
+
+    for i in range(10):
+        if (i in list and i not in list2):
+            displayList.append(i)
+
+    print('Unique list elements: ' + str(displayList) + '\n')
+
+# Exc 4
+def displayOddNumbers():
+    print('Exc 4 - Display odd numbers')
+
+    list = [1,2,3,4,5,6,7,8,9,10]
+    catchElement = []
+
+    # Catch odd numbers and store them in another list
+    for i in list:
+        if (i % 2 != 0):
+            catchElement.append(i)
+
+    print('Found elements: ' + str(catchElement))
+    catchElement.sort()
+    print('Smallest element: ' + str(catchElement[0]) + '\n')
+
+if __name__ == '__main__':
+    main()
