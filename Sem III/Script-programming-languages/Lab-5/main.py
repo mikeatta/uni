fd58e74e12c4cd650e2bc7a2e64e3053b9fcd5ec
mike <saintsikorski@gmail.com>
2022-11-09 21:29:40 +0100

    Fix input validation

--- Sem III/Script-programming-languages/Lab-5/main.py
+++ Sem III/Script-programming-languages/Lab-5/main.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python
 
+# Imports
+import sys
+
 # Exc 1
+# Accepted input range arrays
+binary = ["0", "1"]
+octal = ["0", "1", "2", "3", "4", "5", "6", "7"]
+decimal = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+hexadecimal = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+
+def validateInput(base, input):
+    match base: # Match rangeArray with input
+        case "bin": rangeArray = binary
+        case "oct": rangeArray = octal
+        case "dec": rangeArray = decimal
+        case "hex": rangeArray = hexadecimal
+
+    for i in str(input): # For each character
+        if i not in rangeArray: # Validate input
+            sys.exit("Input out of range")
+        else: return input
+
 def displayConvertedNumbers(numberToConver):
     # Display result from 2nd char onward to hide base id
     print("\nBinary : " + bin(numberToConver)[2:])
@@ -12,28 +33,24 @@ def convertNumber():
     print("Exc 1 - Convert numbers")
     numberBase = input("Enter the number base [bin/oct/dec/hex]: ")
 
-    # Accepted input range arrays
-    binary = ["0", "1"]
-    octal = ["0", "1", "2", "3", "4", "5", "6", "7"]
-
     # Match selected system base
     match numberBase:
 
         case "bin":
-            num = int(input("Enter a binary number: "), 2)
-            for i in (str(num)): # For each character
-                if i not in binary: print("Input out of range"); break # Validate input
+            num = input("Enter a binary number: ")
+            num = int(validateInput(numberBase, num), base=2)
 
         case "oct":
-            num = int(input("Enter an octal number: "), 8)
-            for i in (str(num)): # For each character
-                if i not in octal: print("Input out of range"); break # Validate input
+            num = input("Enter an octal number: ")
+            num = int(validateInput(numberBase, num), base=8)
 
         case "dec":
-            num = int(input("Enter a decimal number: "), 10)
+            num = input("Enter a decimal number: ")
+            num = int(validateInput(numberBase, num), base=10)
 
         case "hex":
-            num = int(input("Enter a hexadecimal number: "), 16)
+            num = input("Enter a hexadecimal number: ")
+            num = int(validateInput(numberBase, num), base=16)
 
         case _:
             print("\nIncorrect input\n\nExiting...")
