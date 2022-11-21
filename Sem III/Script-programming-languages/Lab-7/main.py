317354e99687b0b1e4ed2aa599aecd376d46a38b
mike <saintsikorski@gmail.com>
2022-11-21 09:41:22 +0100

    Add Lab 7 exercise 2

--- Sem III/Script-programming-languages/Lab-7/main.py
+++ Sem III/Script-programming-languages/Lab-7/main.py
@@ -31,8 +31,26 @@ def lineDetails():
     print("Processed text:\n" + fileContent.strip() + "!") # Display text and remove whitespaces from both sides
     f.close() # Close the file after operations
 
+# Exc 2
+def countSpecialCharacters():
+    print("\nExc 2 - Count spaces, tabs and newline characters")
+    f = open("./inwokacja.txt", "r")
+
+    spaceAmount = 0
+    tabAmount = 0
+    newlineAmount = 0
+
+    for line in f:
+        spaceAmount += line.count(" ")
+        tabAmount += line.count("\t")
+        newlineAmount += line.count("\n")
+
+    print("Found [" + str(spaceAmount) + " spaces], [" + str(tabAmount) + " tab signs] and [" + str(newlineAmount) + " newlines]")
+    f.close()
+
 def main():
-    lineDetails() # Exc 1 - Remove whitespaces and count lines and characters
+    # lineDetails() # Exc 1 - Remove whitespaces and count lines and characters
+    countSpecialCharacters() # Exc 2 - Count specific special character occurences
 
 if __name__ == "__main__":
     main()
