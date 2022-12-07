ccde414cc19bcbca62e04ea4e8b4cc904d68b5f9
mike <saintsikorski@gmail.com>
2022-12-07 15:18:08 +0100

    Add Lab 8 exercise 3

--- Sem III/Script-programming-languages/Lab-8/main.py
+++ Sem III/Script-programming-languages/Lab-8/main.py
@@ -17,8 +17,23 @@ def test_regex_uantifiers():
     print(re.findall("a?", example_text))
     print(str(re.findall("a??", example_text)) + "\n")
 
+# Exc 3 - Match parts of an address
+def match_address():
+    print("Exc 2 - Match parts of an address\n")
+
+    # Open and read from file
+    with open("./adresy.txt") as f:
+        file_content = f.read()
+        print(file_content)
+        regex_street = re.findall(r"[a-z]{2}\.[ X]\w+", file_content) # Street name match
+        regex_postcode = re.findall(r"[0-9]{2}.[0-9]{3}", file_content) # Postcode match
+        regex_number = re.findall(r"(\d{1,2}\/\d{1,2}|\s\d{1,2}\s)", file_content) # House number match
+
+    print(str(regex_street) + "\n" + str(regex_postcode) + "\n" + str(regex_number) + "\n")
+
 def main():
-    test_regex_uantifiers() # Exc 1
+    test_regex_uantifiers() # Exc 1 - Test different regex guantifiers
+    match_address() # Exc 3 - Match street name, postcode and house number
 
 if __name__ == "__main__":
     main()
