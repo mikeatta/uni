88c08e55ddcee66d2ce302a2c52d4be64f6d0f61
mike <saintsikorski@gmail.com>
2022-12-12 10:37:52 +0100

    Add creating a file

--- Sem III/Object-oriented-programming/rw-to-file/src/main/java/com/example/rwtofile/HelloController.java
+++ Sem III/Object-oriented-programming/rw-to-file/src/main/java/com/example/rwtofile/HelloController.java
@@ -1,23 +1,31 @@
 package com.example.rwtofile;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+
+import java.io.File;
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-    @FXML
     public TextArea textArea;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 
     @FXML
     private void getTextAreaValue() {
         String text = textArea.getText();
         System.out.println(text);
+    }
+
+    private void createFile() {
+        try {
+            // Create new file at set directory
+            File myFile = new File("./file-directory/file.txt");
+            if(myFile.createNewFile()) {
+                System.out.println("Created new file.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
