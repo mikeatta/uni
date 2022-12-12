4c4a52eeb39a0bea32b24e988bdef0b8c4ca186e
mike <saintsikorski@gmail.com>
2022-12-12 09:34:50 +0100

    Add Lab 9 exercise 2

--- Sem III/Script-programming-languages/Lab-9/main.py
+++ Sem III/Script-programming-languages/Lab-9/main.py
@@ -6,12 +6,26 @@ import numpy as np
 
 # Exc 1 - Replace zeros in matrix
 # Define starting matrix
-mat = np.array([[0,2,3],[4,0,6],[7,8,0,]])
+mat = np.array([[0,2,3],[4,0,6],[7,8,0]])
 
 def replace_zeros(matrix, x):
     print("Exc 1 - Replace zeroes in matrix")
     matrix[matrix == 0] = x
     print(str(matrix) + "\n")
+
+# Exc 2 - Medianize matrix
+def medianize_matrix(matrix):
+    print("Exc 2 - Medianize matrix")
+    mat_mean = matrix.mean()
+
+    # Raise starting matrix to the power of 2
+    mat_power = matrix ** 2
+    print(str(mat_power) + "\n")
+    print("Mean: " + str(mat_mean) + "\n")
+    
+    # Subtract mean from every element
+    matrix = mat_power - mat_mean
+    print(matrix)
 
 # Exc 4 - Test reshape function
 def test_reshape_function(matrix_first_param, matrix_second_param, x):
@@ -19,12 +33,13 @@ def test_reshape_function(matrix_first_param, matrix_second_param, x):
     matrix_first_param = matrix_first_param.reshape(x, 3)
     print(str(matrix_first_param) + "\n")
 
-    matrix_second_param = matrix_second_param.reshape(3, x)
+    matrix_second_param = matrix_second_param.reshape(9, x)
     print(str(matrix_second_param) + "\n")
 
 def main():
     # replace_zeros(mat, 77) # Exc 1 - Replace zeroes in matrix with given number
-    test_reshape_function(mat, mat, -1) # Exc 4 - Test reshape functionality
+    medianize_matrix(mat) # Exc 2 - Medianize matrix
+    # test_reshape_function(mat, mat, -1) # Exc 4 - Test reshape functionality
 
 if __name__ == "__main__":
     main()
