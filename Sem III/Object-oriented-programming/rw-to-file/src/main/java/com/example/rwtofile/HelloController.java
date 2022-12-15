1cb795dc13ebf884b5aa90c1ad165eca41ad099e
mike <saintsikorski@gmail.com>
2022-12-15 15:26:38 +0100

    Read and write to file
    Add checking for file existence, add handling for reading from empty file, add writing textArea content to file.

--- Sem III/Object-oriented-programming/rw-to-file/src/main/java/com/example/rwtofile/HelloController.java
+++ Sem III/Object-oriented-programming/rw-to-file/src/main/java/com/example/rwtofile/HelloController.java
@@ -3,32 +3,63 @@ package com.example.rwtofile;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 public class HelloController {
+
+    // Define file path
+    String filePath = "./file-directory/file.txt";
     @FXML
     public TextArea textArea;
 
     @FXML
-    private void getTextAreaValue() {
-        String text = textArea.getText();
-        System.out.println(text);
-        createFile();
+    private void readFromFile() throws FileNotFoundException {
+        // If file is present, read from file
+        if (checkForFile()) {
+            File myFile = new File(filePath);
+            Scanner sc = new Scanner(myFile);
+            sc.useDelimiter("\\Z");
+
+            if (sc.hasNext() == false) {
+                // Let user know the file has no content
+                System.out.println("File is empty.");
+            } else textArea.setText(sc.next());
+        } else checkForFile();
     }
 
-    private void createFile() {
+    @FXML
+    private void writeToFile() {
+        // Get text from input field
+        String text = textArea.getText();
+
         try {
-            // Create new file at set directory
-            File myFile = new File("./file-directory/file.txt");
-            // If file exists and is not a directory
-            if(myFile.exists() && !myFile.isDirectory()) {
-                System.out.println("File already exists! Aborting...");
-            } else myFile.createNewFile();
+            // Write to file
+            FileWriter myWriter = new FileWriter(filePath);
+            myWriter.write(text);
+            // Close the file
+            myWriter.close();
+            System.out.println("Successfully written to file!");
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+    }
+
+    private boolean checkForFile() {
+        try {
+            File myFile = new File(filePath);
+            // Return false, if file was not found
+            if ((!myFile.exists() || myFile.isDirectory()) && myFile.createNewFile()) {
+                System.out.println("File not found!\nCreating a new file!");
+                return false;
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        // Return true, if file was found
+        return true;
     }
 
 }
