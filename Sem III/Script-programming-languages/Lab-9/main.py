a23837d991fcf88aa35dba7661e6a44176e1bb55
mike <saintsikorski@gmail.com>
2022-12-16 21:13:04 +0100

    Add Lab 9 exercise 8

--- Sem III/Script-programming-languages/Lab-9/main.py
+++ Sem III/Script-programming-languages/Lab-9/main.py
@@ -25,7 +25,7 @@ def medianize_matrix(matrix):
     
     # Subtract mean from every element
     matrix = mat_power - mat_mean
-    print(matrix)
+    print(str(matrix) + "\n")
 
 # Exc 4 - Test reshape function
 def test_reshape_function(matrix_first_param, matrix_second_param, x):
@@ -36,10 +36,24 @@ def test_reshape_function(matrix_first_param, matrix_second_param, x):
     matrix_second_param = matrix_second_param.reshape(9, x)
     print(str(matrix_second_param) + "\n")
 
+# Exc 8 - Sum matrix & count occurances
+def sum_and_count_matrix():
+    print("Exc 8 - Sum elements and count occurances")
+
+    matrix = np.random.randint(10, size=(10,10))
+    print(str(matrix) + "\n")
+
+    print("Sum: " + str(matrix.sum()))
+
+    print("Occurances: ")
+    for occurance in range(10):
+        print(f"[{str(occurance)}] : {str(np.count_nonzero(matrix == occurance))}")
+        
 def main():
-    # replace_zeros(mat, 77) # Exc 1 - Replace zeroes in matrix with given number
+    replace_zeros(mat, 77) # Exc 1 - Replace zeroes in matrix with given number
     medianize_matrix(mat) # Exc 2 - Medianize matrix
-    # test_reshape_function(mat, mat, -1) # Exc 4 - Test reshape functionality
+    test_reshape_function(mat, mat, -1) # Exc 4 - Test reshape functionality
+    sum_and_count_matrix() # Exc 8 - Sum elements and count occurances
 
 if __name__ == "__main__":
     main()
