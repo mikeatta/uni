a4dd6f5d10e79a4bad39951a14860f21cf876acc
mike <saintsikorski@gmail.com>
2022-12-18 21:22:47 +0100

    Fix typo

--- Sem III/Script-programming-languages/Lab-9/main.py
+++ Sem III/Script-programming-languages/Lab-9/main.py
@@ -36,24 +36,24 @@ def test_reshape_function(matrix_first_param, matrix_second_param, x):
     matrix_second_param = matrix_second_param.reshape(9, x)
     print(str(matrix_second_param) + "\n")
 
-# Exc 8 - Sum matrix & count occurances
+# Exc 8 - Sum matrix & count occurrences
 def sum_and_count_matrix():
-    print("Exc 8 - Sum elements and count occurances")
+    print("Exc 8 - Sum elements and count occurrences")
 
     matrix = np.random.randint(10, size=(10,10))
     print(str(matrix) + "\n")
 
     print("Sum: " + str(matrix.sum()))
 
-    print("Occurances: ")
-    for occurance in range(10):
-        print(f"[{str(occurance)}] : {str(np.count_nonzero(matrix == occurance))}")
+    print("Occurrences: ")
+    for occurrence in range(10):
+        print(f"[{str(occurrence)}] : {str(np.count_nonzero(matrix == occurrence))}")
         
 def main():
     replace_zeros(mat, 77) # Exc 1 - Replace zeroes in matrix with given number
     medianize_matrix(mat) # Exc 2 - Medianize matrix
     test_reshape_function(mat, mat, -1) # Exc 4 - Test reshape functionality
-    sum_and_count_matrix() # Exc 8 - Sum elements and count occurances
+    sum_and_count_matrix() # Exc 8 - Sum elements and count occurrences
 
 if __name__ == "__main__":
     main()
