7f8c2956c481ffa0f17f592dcdfdb538adf7bfbd
mike <saintsikorski@gmail.com>
2023-01-03 15:30:11 +0100

    Reorganize exception-specific classes

--- Sem III/Object-oriented-programming/java-exceptions/src/main/java/com/example/javaexceptions/ExceptionHandler.java
+++ Sem III/Object-oriented-programming/java-exceptions/src/main/java/com/example/javaexceptions/ExceptionHandler.java
@@ -9,38 +9,36 @@ public class ExceptionHandler extends Exception {
     }
 }
 
-// Empty TextField exceptions
+// Card number TextField exceptions
 class CardNumberEmpty extends ExceptionHandler {
     public CardNumberEmpty(String message) {
         super(message);
     }
 }
-
-class ExpiryDateEmpty extends ExceptionHandler {
-    public ExpiryDateEmpty(String message) {
-        super(message);
-    }
-}
-
-class CvvEmpty extends ExceptionHandler {
-    public CvvEmpty(String message) {
-        super(message);
-    }
-}
-
-// Incorrect TextField length exceptions
 class IncorrectCardLength extends ExceptionHandler {
     public IncorrectCardLength(String message) {
         super(message);
     }
 }
 
+// Expiry date TextField exceptions
+class ExpiryDateEmpty extends ExceptionHandler {
+    public ExpiryDateEmpty(String message) {
+        super(message);
+    }
+}
 class IncorrectDateLength extends ExceptionHandler {
     public IncorrectDateLength(String message) {
         super(message);
     }
 }
 
+// Cvv code TextField exceptions
+class CvvEmpty extends ExceptionHandler {
+    public CvvEmpty(String message) {
+        super(message);
+    }
+}
 class IncorrectCvvLength extends ExceptionHandler {
     public IncorrectCvvLength(String message) {
         super(message);
