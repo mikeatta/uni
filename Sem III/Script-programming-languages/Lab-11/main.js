7850aaa9f3ef2d7519165c71f4ffed9fa49df44a
mike <saintsikorski@gmail.com>
2023-01-09 09:00:37 +0100

    Add Lab 11 exercise 2

---
+++ Sem III/Script-programming-languages/Lab-11/main.js
@@ -0,0 +1,14 @@
+// Exc 2 - Store string in variable & test string operations
+console.log("Exc 2 - String operations");
+
+let string = '“Pan Wołodyjowski” przedstawia losy pana Michała - najlepszej szabli Rzeczpospolitej.';
+
+// Return string length
+console.log(`Total string length : ${string.length}`);
+
+// Return first index of the word
+console.log(`First index of word at : ${string.indexOf("szabli")}`);
+
+// Replace words in string
+let replacedWordString = string.replace("najlepszej", "pierwszej");
+console.log(`Altered string : ${replacedWordString}`);
