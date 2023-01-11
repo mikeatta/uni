4bc3dc9dab069d10ce0c6b1e5c0bf41a6b28a5b4
mike <saintsikorski@gmail.com>
2023-01-11 22:35:57 +0100

    Add Lab 11 exercise 3
    Add exc 3 and split code into NodeJS and browser-executed parts.

--- Sem III/Script-programming-languages/Lab-11/main.js
+++ Sem III/Script-programming-languages/Lab-11/main.js
@@ -1,6 +1,4 @@
-// Exc 1 - Create an alert pop-up on the webpage
-let popUpDialog = "Hello world";
-alert(popUpDialog);
+// --- NodeJS code ---
 
 // Exc 2 - Store string in variable & test string operations
 console.log("Exc 2 - String operations");
@@ -15,4 +13,29 @@ console.log(`First index of word at : ${string.indexOf("szabli")}`);
 
 // Replace words in string
 let replacedWordString = string.replace("najlepszej", "pierwszej");
-console.log(`Altered string : ${replacedWordString}`);
+console.log(`Altered string : ${replacedWordString}\n`);
+
+// Exc 3 - Split string into words & display words backwards
+console.log("Exc 3 - Split string & display backwards in alert pop-up\n...")
+
+string = "Turków. przez Podolskiego Kamieńca przejęcie i Oblężenie";
+
+// Split the string using &nbsp as separator
+splitString = string.split(" ").reverse();
+
+let alertString = "";
+for (i=0; i<splitString.length; i++) {
+    alertString += splitString[i];
+    alertString += " ";
+}
+
+console.log(`Reverse string content : ${alertString}\n`);
+
+// --- Browser code ---
+
+// // Exc 1 - Create an alert pop-up on the webpage
+let popUpDialog = "Hello world";
+alert(popUpDialog);
+
+// Exc 3 - Split string into words & display words backwards
+alert(alertString);
