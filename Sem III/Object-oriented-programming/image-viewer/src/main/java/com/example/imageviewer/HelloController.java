bc8180187dd1dbeb27607d9830556e64131dd099
mike <saintsikorski@gmail.com>
2023-01-16 11:27:37 +0100

    Display images in cycle

--- Sem III/Object-oriented-programming/image-viewer/src/main/java/com/example/imageviewer/HelloController.java
+++ Sem III/Object-oriented-programming/image-viewer/src/main/java/com/example/imageviewer/HelloController.java
@@ -6,6 +6,8 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.util.ArrayList;
+
 public class HelloController {
     @FXML
     private Label imageCounter;
@@ -14,19 +16,31 @@ public class HelloController {
     @FXML
     private ImageView imageView;
 
+    // Program variables
+    String imgDirectoryPath = "/home/zorin/Documents/git/uni/Sem III/Object-oriented-programming/image-viewer/img/";
+    ArrayList<String> images = new ArrayList<>();
     int imageCount = 0;
 
     @FXML
     protected void loadImage() {
+        // Load images into array list
+        if (imageCount == 0) {
+            images.add(imgDirectoryPath + "img-1.jpg");
+            images.add(imgDirectoryPath + "img-2.jpg");
+            images.add(imgDirectoryPath + "img-3.jpg");
+            images.add(imgDirectoryPath + "img-4.jpg");
+        }
+
         // Increment image counter
         imageCount++;
 
         // Load the image
-        Image image = new Image("");
+        Image image = new Image("file:" + images.get(imageCount - 1));
 
         // Display the image via Image View
         imageView.setImage(image);
-        imageView.setFitWidth(100);
+        imageView.setFitWidth(300);
+        imageView.setFitHeight(400);
         imageView.setPreserveRatio(true);
         imageView.setSmooth(true);
         imageView.setCache(true);
