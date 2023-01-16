7d700170477979008af488a58ce9e93d62a2de0a
mike <saintsikorski@gmail.com>
2023-01-16 21:21:28 +0100

    Remove misplaced image-viewer directory

--- Sem III/image-viewer/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.imageviewer {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.imageviewer to javafx.fxml;
-    exports com.example.imageviewer;
-}
