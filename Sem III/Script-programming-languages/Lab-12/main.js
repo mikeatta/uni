c5daaf6d6d479d39f969a7eced8b0d693378d8d7
mike <saintsikorski@gmail.com>
2023-01-18 21:56:50 +0100

    Add button move-in & move-out events

--- Sem III/Script-programming-languages/Lab-12/main.js
+++ Sem III/Script-programming-languages/Lab-12/main.js
@@ -18,3 +18,26 @@ function validateCheckBox() {
         label.style.backgroundColor = "#ffffff";
     }
 }
+
+// Exc 3 - Add actions to a button
+// Content selectors
+var paragraphSelector = document.getElementById("action-paragraph");
+var divBackground = document.querySelector("#action-div");
+
+// Hover in event
+document.getElementById("action-button").addEventListener("mouseover", () => {
+    divBackground.style.backgroundColor = "hsl(240, 3%, 94%)";
+    paragraphSelector.style.fontSize = "18px";
+    paragraphSelector.style.fontStyle = "oblique";
+    paragraphSelector.style.backgroundColor = "hsl(204, 8%, 76%)";
+    paragraphSelector.style.color = "hsl(0, 100%, 100%)";
+});
+
+// Hover out event
+document.getElementById("action-button").addEventListener("mouseleave", () => {
+    divBackground.style.backgroundColor = "hsl(0, 100%, 100%)";
+    paragraphSelector.style.fontSize = "16px";
+    paragraphSelector.style.fontStyle = "normal";
+    paragraphSelector.style.backgroundColor = "hsl(0, 100%, 100%)";
+    paragraphSelector.style.color = "hsl(0, 0%, 0%)";
+})
