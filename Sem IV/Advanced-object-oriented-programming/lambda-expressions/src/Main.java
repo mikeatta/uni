2d6eab8e059b906227e5597bb121537b59568a86
mike <saintsikorski@gmail.com>
2023-03-11 18:46:52 +0100

    Add example of constructor reference

--- Sem IV/Advanced-object-oriented-programming/lambda-expressions/src/Main.java
+++ Sem IV/Advanced-object-oriented-programming/lambda-expressions/src/Main.java
@@ -31,5 +31,24 @@ public class Main {
         CheckForCharacter checkForCharacter = new CheckForCharacter();
         BiFunction<Character, String, Boolean> findChar = checkForCharacter::searchForCharacter;
         System.out.println("String contains the character: " + findChar.apply('c', "char"));
+
+        System.out.println("-----------------");
+
+        // Constrictor reference
+        class Multiply {
+            private final Integer base;
+            private final Integer exponent;
+
+            public Multiply(Integer base, Integer exponent) {
+                this.base = base;
+                this.exponent = exponent;
+            }
+
+            public Integer getResult() {
+                return this.base * this.exponent;
+            }
+        }
+        BiFunction<Integer, Integer, Multiply> multiply = Multiply::new;
+        System.out.println("Operation result: " + multiply.apply(10, 10).getResult());
     }
 }
