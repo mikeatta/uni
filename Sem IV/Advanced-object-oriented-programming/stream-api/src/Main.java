892abee6f8aa8bfc5353511a7ed9471186b2d92a
mike <saintsikorski@gmail.com>
2023-03-15 13:40:11 +0100

    Add filtering even elements of the ArrayList

--- Sem IV/Advanced-object-oriented-programming/stream-api/src/Main.java
+++ Sem IV/Advanced-object-oriented-programming/stream-api/src/Main.java
@@ -7,12 +7,21 @@ public class Main {
 
         System.out.println("--------------");
 
+        // Get the smallest element of storeIntegers
+        System.out.println("Get smallest element:");
         ArrayList<Integer> storeIntegers = new ArrayList<>();
         storeIntegers.add(7);
         storeIntegers.add(14);
         storeIntegers.add(21);
 
-        Stream<Integer> integerStream = storeIntegers.stream();
-        System.out.println(integerStream.min(Integer::compare));
+        Stream<Integer> smallestInteger = storeIntegers.stream();
+        System.out.println(smallestInteger.min(Integer::compare));
+
+        System.out.println("--------------");
+
+        // Get even elements of storeIntegers
+        System.out.println("Get even elements:");
+        Stream<Integer> evenIntegers = storeIntegers.stream();
+        evenIntegers.filter(integer -> integer % 2 == 0).forEach(integer -> System.out.println(integer));
     }
 }
