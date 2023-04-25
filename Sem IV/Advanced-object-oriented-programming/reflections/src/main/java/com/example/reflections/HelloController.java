15c1fd45f7321fac88f53a9ab4ab8d4c2bfaa25c
mike <saintsikorski@gmail.com>
2023-04-25 22:22:20 +0200

    Add test TextField and Label

--- Sem IV/Advanced-object-oriented-programming/reflections/src/main/java/com/example/reflections/HelloController.java
+++ Sem IV/Advanced-object-oriented-programming/reflections/src/main/java/com/example/reflections/HelloController.java
@@ -2,7 +2,11 @@ package com.example.reflections;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
@@ -13,6 +17,8 @@ public class HelloController {
     private Label welcomeText;
     @FXML
     private TextField classNameField;
+    @FXML
+    private VBox objectPropertyMenu;
 
     @FXML
     protected void onHelloButtonClick() {
@@ -48,9 +54,24 @@ public class HelloController {
         for (Field field : fields) {
             System.out.println(field.getName());
         }
+
+        createObjectPropertiesMenu();
     }
 
     @FXML
     private void saveChanges() {}
 
+    // TODO: Add and format TextArea and a Label
+    @FXML
+    private void createObjectPropertiesMenu() {
+
+        TextField value = new TextField();
+        Label description = new Label();
+
+        value.setMaxWidth(300);
+
+        objectPropertyMenu.getChildren().add(new SplitPane(value, description));
+    }
+
+    private void displayObjectProperties() {}
 }
