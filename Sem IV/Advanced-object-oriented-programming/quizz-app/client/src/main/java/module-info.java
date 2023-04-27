d5674f582861039b584cdccccbde7c95f3cbf909
mike <saintsikorski@gmail.com>
2023-04-27 19:48:14 +0200

    Remove old project files
    The old project was outdated, not working properly and did not contain
    the most recent changes. Deleting these files helps make the directory
    cleaner.

--- Sem IV/Advanced-object-oriented-programming/quizz-app/client/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.client {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.example.quizzapp;
-
-
-    opens com.example.client to javafx.fxml;
-    exports com.example.client;
-}
--- Sem IV/Advanced-object-oriented-programming/quizz-app/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.quizzapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.quizzapp to javafx.fxml;
-    exports com.example.quizzapp;
-}
