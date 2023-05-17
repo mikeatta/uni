305e38770ab51cf7127a3c969f9098a7150bcc79
mike <saintsikorski@gmail.com>
2023-05-17 15:12:41 +0200

    Add validation icons and tooltip
    Display icons for correct and incorrect input. Display a tooltip with a
    status message on incorrect input.

--- Sem IV/Advanced-object-oriented-programming/validation-annotations/src/main/java/com/example/validationannotations/VinputText.java
+++ Sem IV/Advanced-object-oriented-programming/validation-annotations/src/main/java/com/example/validationannotations/VinputText.java
@@ -1,13 +1,24 @@
 package com.example.validationannotations;
 
 import javafx.scene.control.TextInputControl;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
+import javafx.util.Duration;
+
+import java.util.Objects;
 
 public class VinputText extends HBox {
 
     private TextInputControl inputControl;
     private Validator patternValidator;
     private final HelloController helloController;
+    private final Tooltip tooltip = new Tooltip();
+
+    Image checkmark = new Image(Objects.requireNonNull(getClass()
+            .getResourceAsStream("/icons/checkmark.png")));
+    Image cross = new Image(Objects.requireNonNull(getClass()
+            .getResourceAsStream("/icons/cross.png")));
 
     public VinputText(HelloController helloController) {
         this.helloController = helloController;
@@ -33,12 +44,16 @@ public class VinputText extends HBox {
     }
 
     private void confirmValidation() {
+        Tooltip.uninstall(helloController.getImageView(), tooltip);
+        helloController.getImageView().setImage(checkmark);
         helloController.getConfirmButton().setDisable(false);
-        System.out.println("Field content valid");
     }
 
     private void rejectValidation() {
+        Tooltip.install(helloController.getImageView(), tooltip);
+        tooltip.setShowDelay(new Duration(0.3));
+        tooltip.setText(patternValidator.getMessage());
+        helloController.getImageView().setImage(cross);
         helloController.getConfirmButton().setDisable(true);
-        System.out.println("Field content invalid");
     }
 }
