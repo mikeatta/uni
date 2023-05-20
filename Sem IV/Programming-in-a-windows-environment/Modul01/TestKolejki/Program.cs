b4a354aad8725217a920bae04c6957d96201d98e
mike <saintsikorski@gmail.com>
2023-05-20 13:13:12 +0200

    Fix faulty import
    Replace Program.cs with a correctly linked file.
    
    The old file was being linked directly from the exercise instructions
    directory and had not been committed to the repository because of it.

--- Sem IV/Programming-in-a-windows-environment/Modul01/TestKolejki/Program.cs
+++ Sem IV/Programming-in-a-windows-environment/Modul01/TestKolejki/Program.cs
@@ -1,3 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
+using KolejkaFIFO;
 
-Console.WriteLine("Hello, World!");
+namespace TestKolejki
+{
+    class Program
+    {
+        static char Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("\n\t\tA - Dodaj napis do kolejki");
+            Console.WriteLine("\n\t\tB - Usuń napis z kolejki");
+            Console.WriteLine("\n\t\tC - Podaj liczbę elementów w kolejce");
+            Console.WriteLine("\n\t\tD - Wyczyść kolejkę");
+            Console.WriteLine("\n\t\tK - Koniec");
+            return Console.ReadKey(true).KeyChar;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Kolejka mojaKolejka= new Kolejka();
+            mojaKolejka.Utworz(10);
+            string tmp;
+            char c;
+
+            do
+            {
+                c = Menu();
+                switch (c)
+                {
+                    case 'a':
+                    case 'A':
+                        Console.Write("Podaj napis który ma być dodany do kolejki: ");
+                        tmp = Console.ReadLine();
+                        mojaKolejka.DodajDoKolejki(tmp);
+                        break;
+                    case 'b':
+                    case 'B':
+                        Console.WriteLine("Napis wyjęty z kolejki: {0}",
+                            mojaKolejka.UsunZKolejki());
+                        Console.ReadKey();
+                        break;
+                    case 'c':
+                    case 'C':
+                        Console.WriteLine("Liczba elementów w kolejce wynosi: {0}", 
+                            mojaKolejka.PobierzLiczbeElementow());
+                        Console.ReadKey();
+                        break;
+                    case 'd':
+                    case 'D':
+                        mojaKolejka.Wyczysc();
+                        Console.WriteLine("Kolejka wyczyszczona!!!");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+            while (!(c == 'k' || c == 'K'));
+        }
+    }
+}
