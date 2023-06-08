86f4929adf8e6234b8c544a3635e6985f89aea47
mike <saintsikorski@gmail.com>
2023-06-08 14:38:45 +0200

    Add Problem3 from Lab07

---
+++ Sem IV/Programming-in-a-windows-environment/Modul07/Samochody/Samochod.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Samochody
+{
+    public class Silnik
+    {
+        private double pojemnosc;
+        public double Pojemnosc
+        {
+            get { return pojemnosc; }
+        }
+        private  readonly int numerFabryczny;
+        public int NumerFabryczny
+        {
+            get { return numerFabryczny; }
+        }
+        public Silnik(double pojemnosc, int numerFabryczny)
+        {
+            this.numerFabryczny = numerFabryczny;
+            this.pojemnosc = pojemnosc;
+        }
+    }
+
+    public class Osoba
+    {
+        public string Nazwisko { set; get; }
+        public string Imie { set; get; }
+    }
+
+    public class Samochod
+    {
+        public Osoba Wlasciciel { set; get; }
+
+        private readonly int rokProdukcji;
+        public int RokProdukcji
+        {
+            get { return rokProdukcji; }
+        }
+
+        private  readonly string marka;
+        public string Marka
+        {
+            get { return marka; }
+        }
+
+        private readonly string model;
+        public string Model
+        {
+            get { return model; }
+        }
+
+        public Silnik Silnik { set; get; }
+
+        public Samochod(Osoba wlasciciel, int rokProdukcji, string marka,
+            string producent, Silnik silnik)
+        {
+            Wlasciciel = wlasciciel;
+            this.rokProdukcji = rokProdukcji;
+            this.marka = marka;
+            this.model = producent;
+            Silnik = silnik;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1} rok produkcji: {2}; pojemność " +
+                                 "silnika: {3}; numer silnika: {4}; " +
+                                 "właściciel: {5} {6}", Model, Marka,
+                RokProdukcji, Silnik.Pojemnosc, Silnik.NumerFabryczny,
+                Wlasciciel.Imie, Wlasciciel.Nazwisko);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (this.GetType() != obj.GetType()) return false;
+            Samochod s = (Samochod)obj;
+            if (RokProdukcji != s.RokProdukcji) return false;
+            if (!string.Equals(Model, s.Model)) return false;
+            if (!string.Equals(Marka, s.Marka)) return false;
+            if (!object.Equals(Silnik, s.Silnik)) return false;
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return string.Format("{0} {1} {2} {3}", Model, Marka, RokProdukcji,
+                Silnik.NumerFabryczny, Silnik.Pojemnosc).GetHashCode();
+        }
+    }
+}
---
+++ Sem IV/Programming-in-a-windows-environment/Modul07/TestSamochodu/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Samochody;
+
+namespace TestSamochodu
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Silnik silnik1 = new Silnik(2.2, 123);
+            Silnik silnik2 = new Silnik(2.2, 123);
+            Osoba osoba1 = new Osoba() { Nazwisko = "Kowalski", Imie = "Jan" };
+            Osoba osoba2 = new Osoba() { Nazwisko = "Nowak", Imie = "Paweł" };
+
+            Samochod s1 = new Samochod(osoba1, 2001, "A2", "Audi", silnik1);
+            Samochod s2 = new Samochod(osoba2, 2001, "A2", "Audi", silnik1);
+            Samochod s3 = new Samochod(osoba1, 2001, "A2", "Audi", silnik2);
+            
+            Console.WriteLine("Samochód s1: {0}", s1);
+            Console.WriteLine("Samochód s2: {0}", s2);
+            Console.WriteLine("Samochód s3: {0}\n", s3);
+            
+            Console.WriteLine("Samochód s1 kod skrótu: {0}", s1.GetHashCode());
+            Console.WriteLine("Samochód s2 kod skrótu: {0}", s2.GetHashCode());
+            Console.WriteLine("Samochód s3 kod skrótu: {0}\n",
+                s3.GetHashCode());
+            
+            Console.WriteLine("s1 i s2 są sobie równe to: {0}", s1.Equals(s2));
+            Console.WriteLine("s1 i s3 są sobie równe to: {0}", s1.Equals(s3));
+            Console.WriteLine("s2 i s3 są sobie równe to: {0}", s2.Equals(s3));
+
+            Console.ReadKey();
+        }
+    }
+}
