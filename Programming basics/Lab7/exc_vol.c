1311e5d99e717f1fcee072a74db3d601ac2fe2ef
mike <saintsikorski@gmail.com>
2023-06-09 13:17:05 +0200

    Add number histogram

--- Programming basics/Lab7/exc_vol.c
+++ Programming basics/Lab7/exc_vol.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 #define LETTERS 26
+#define NUMBERS 10
 
-void create_histogram(const char* str, int histogram[])
+void create_letter_histogram(const char* str, int histogram[])
 {
     int i = 0;
 
@@ -22,21 +23,54 @@ void create_histogram(const char* str, int histogram[])
     }
 }
 
+void create_number_histogram(const char* str, int histogram[])
+{
+    int i = 0;
+
+    for (i = 0; i < NUMBERS; i++)
+    {
+        histogram[i] = 0;
+    }
+
+    while (*str)
+    {
+        char ch = *str;
+        if (ch >= '0' && ch <= '9')
+        {
+            histogram[ch - '0']++;
+        }
+        str++;
+    }
+}
+
 int main()
 {
-    const char* str = "Hello, world!";
-    int histogram[LETTERS];
+    const char* str = "Hello, world! 123";
+    int letter_arr[LETTERS];
+    int number_arr[NUMBERS];
 
-    create_histogram(str, histogram);
+    create_letter_histogram(str, letter_arr);
+    create_number_histogram(str, number_arr);
 
-    printf("Character occurence histogram:\n");
+    printf("Letter histogram:\n");
     printf("------------------------------\n");
     for (int i = 0; i < LETTERS; i++)
     {
-        if (histogram[i] > 0)
+        if (letter_arr[i] > 0)
         {
             char ch = 'a' + i;
-            printf("%c : %d\n", ch, histogram[i]);
+            printf("%c : %d\n", ch, letter_arr[i]);
+        }
+    }
+
+    printf("\nNumber histogram:\n");
+    printf("------------------------------\n");
+    for (int i = 0; i < NUMBERS; i++)
+    {
+        if (number_arr[i] > 0)
+        {
+            char ch = '0' + i;
+            printf("%c : %d\n", ch, number_arr[i]);
         }
     }
 
