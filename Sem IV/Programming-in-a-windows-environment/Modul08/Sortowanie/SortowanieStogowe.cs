75a2f5128c7ab0b51e664ec1ee20fb4cc1d906f9
mike <saintsikorski@gmail.com>
2023-06-12 14:58:19 +0200

    Add Problem1 from Lab08

---
+++ Sem IV/Programming-in-a-windows-environment/Modul08/Sortowanie/SortowanieStogowe.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Sortowanie
+{
+    public static class SortowanieStogowe
+    {
+        private static void Przesiewane<T>(T[] tablica, int lewy, int prawy)
+        where T : IComparable<T>
+        {
+            int i = lewy, j = 2 * i + 1;
+            T x = tablica[i];
+            while (j <= prawy)
+            {
+                if (j < prawy)
+                {
+                    if (tablica[i].CompareTo(tablica[j + 1]) < 0)
+                        j = j + 1;
+                }
+                if (tablica[j].CompareTo(x) < 0)
+                    break;
+
+                tablica[i] = tablica[j];
+                i = j;
+                j = 2 * i + 1;
+            }
+
+            tablica[i] = x;
+        }
+
+        public static void Sortuj<T>(T[] tablica) where T : IComparable<T>
+        {
+            int l = tablica.Length / 2, p = tablica.Length - 1;
+            T x;
+            while (l > 0)
+            {
+                l--;
+                Przesiewane(tablica, l, p);
+            }
+
+            while (p > 0)
+            {
+                x = tablica[0];
+                tablica[0] = tablica[p];
+                tablica[p] = x;
+                p--;
+                Przesiewane(tablica, 0, p);
+            }
+        }
+    }
+}
---
+++ Sem IV/Programming-in-a-windows-environment/Modul08/TestSortowania/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Sortowanie;
+
+namespace TestSortowania
+{
+    class Osoba
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            double[] tab1 = { 22.3, 3.5, 1, 7.8, 6.5 };
+            string[] imiona = { "Paweł", "Ania", "Karol", "Piotr" };
+            
+            SortowanieStogowe.Sortuj(tab1);
+            SortowanieStogowe.Sortuj(imiona);
+            
+            Console.Write("Imiona: ");
+            foreach (string imie in imiona)
+            {
+                Console.Write($"{imie} ");
+            }
+            
+            Console.Write("\nLiczby: ");
+            foreach (double number in tab1)
+            {
+                Console.Write($"{number} ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
