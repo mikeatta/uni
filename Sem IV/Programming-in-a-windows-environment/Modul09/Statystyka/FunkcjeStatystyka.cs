e3dfefa7862cf4faabd9362071d8164db1be775c
mike <saintsikorski@gmail.com>
2023-06-13 18:47:45 +0200

    Add Problem1 from Lab09

---
+++ Sem IV/Programming-in-a-windows-environment/Modul09/Statystyka/FunkcjeStatystyka.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Statystyka
+{
+    public static class FunkcjeStatystyka
+    {
+        public static int Ilosc(IEnumerable<double> tab,
+            Func<double, bool> kryterium)
+        {
+            int i = 0;
+            foreach (double d in tab)
+            {
+                if (kryterium(d))
+                    i++;
+            }
+
+            return i;
+        }
+
+        public static double Suma(IEnumerable<double> tab,
+            Func<double, bool> kryterium)
+        {
+            double suma = 0;
+            foreach (double d in tab)
+            {
+                if (kryterium(d))
+                    suma += d;
+            }
+
+            return suma;
+        }
+
+        public static double SredniaArytmetyczna(IEnumerable<double> tab,
+            Func<double, bool> kryterium)
+        {
+            double suma = 0;
+            int i = 0;
+            foreach (double d in tab)
+            {
+                if (kryterium(d))
+                {
+                    suma += d;
+                    i++;
+                }
+            }
+
+            if (i == 0)
+            {
+                throw new ArgumentException("Żaden element kolekcji nie " +
+                                                        "spełnia kryterium.");
+            }
+
+            return suma / i;
+        }
+
+        public static double Maksimum(IEnumerable<double> tab,
+            Func<double, bool> kryterium)
+        {
+            bool znaleziono = false;
+            double max = double.MinValue;
+            foreach (double f in tab)
+            {
+                if (kryterium(f) && (f > max || !znaleziono))
+                {
+                    max = f;
+                    znaleziono = true;
+                }
+            }
+
+            if (!znaleziono)
+            {
+                throw new InvalidOperationException("Nie znaleziono żadnego " +
+                                "elementu, który spełnia zadane kryterium.");
+            }
+
+            return max;
+        }
+
+        public static double Minimum(IEnumerable<double> tab,
+            Func<double, bool> kryterium)
+        {
+            bool znaleziono = false;
+            double min = double.MaxValue;
+            foreach (double f in tab)
+            {
+                if (kryterium(f) && (f < min || !znaleziono))
+                {
+                    min = f;
+                    znaleziono = true;
+                }
+            }
+
+            if (!znaleziono)
+            {
+                throw new InvalidOperationException("Nie znaleziono " +
+                        "żadnego elementu, który spełnia zadane kryterium.");
+            }
+
+            return min;
+        }
+    }
+}
---
+++ Sem IV/Programming-in-a-windows-environment/Modul09/TestFunkcjiStatycznych/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Statystyka;
+
+namespace TestFunkcjiStatycznych
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            double[] tab = { 2, 3, 6, 9 };
+            Console.WriteLine("Liczba elementów tablicy większych od " +
+                        $"pięciu {FunkcjeStatystyka.Ilosc(tab, p => p > 5)}");
+            
+            Console.WriteLine("Suma elementów tablicy większych od " +
+                        $"pięciu {FunkcjeStatystyka.Suma(tab, p => p > 5)}");
+            
+            Console.WriteLine("Średnia elementów tablicy większych od " +
+                        $"pięciu: {FunkcjeStatystyka.SredniaArytmetyczna(tab, p => p > 5)}");
+            
+            Console.WriteLine("Największy z elementów tablicy większych od " +
+                        $"pięciu: {FunkcjeStatystyka.Maksimum(tab, p => p > 5)}");
+            
+            Console.WriteLine("Najmniejszy z elementów tablicy większych od " +
+                        $"pięciu: {FunkcjeStatystyka.Minimum(tab, p => p > 5)}");
+
+            Console.ReadKey();
+        }
+    }
+}
