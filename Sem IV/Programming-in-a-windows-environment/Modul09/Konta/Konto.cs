61da7f7fa3edfe59937e8b604c99e6e5837e9d22
mike <saintsikorski@gmail.com>
2023-06-15 23:02:37 +0200

    Add Problem2 from Lab09

---
+++ Sem IV/Programming-in-a-windows-environment/Modul09/Konta/Konto.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Konta
+{
+    public class Konto
+    {
+        private string haslo;
+        public string NazwaUzytkownika { set; get; }
+
+        public Konto(string nazwaUzytkownika, string haslo)
+        {
+            NazwaUzytkownika = nazwaUzytkownika;
+            this.haslo = haslo;
+        }
+
+        public static event PrzedZmianaHasloHandler PrzedZmianaHasla;
+        public static event EventHandler PoZmianieHasla;
+        
+        public bool SprawdzHaslo(string haslo)
+        {
+            if (this.haslo == haslo)
+                return true;
+            return false;
+        }
+
+        public bool ZmienHaslo(string stareHaslo, string noweHaslo)
+        {
+            if (!SprawdzHaslo(stareHaslo))
+                return false;
+
+            if (PrzedZmianaHasla != null)
+            {
+                PrzedZmianaHaslaArgs e = new PrzedZmianaHaslaArgs(noweHaslo,
+                    stareHaslo);
+                PrzedZmianaHasla(this, e);
+                if (e.Cancel)
+                    return false;
+            }
+
+            haslo = noweHaslo;
+            if (PoZmianieHasla != null)
+                PoZmianieHasla(this, EventArgs.Empty);
+            return true;
+        }
+    }
+
+    public class PrzedZmianaHaslaArgs : CancelEventArgs
+    {
+        private string _noweHaslo;
+
+        public string NoweHaslo
+        {
+            get { return _noweHaslo; }
+        }
+
+        private string _stareHaslo;
+
+        public string StareHaslo
+        {
+            get { return _stareHaslo; }
+        }
+
+        public PrzedZmianaHaslaArgs(string noweHaslo, string stareHaslo)
+        {
+            this._noweHaslo = noweHaslo;
+            this._stareHaslo = stareHaslo;
+        }
+    }
+
+    public delegate void PrzedZmianaHasloHandler(
+        object sender, PrzedZmianaHaslaArgs e);
+}
---
+++ Sem IV/Programming-in-a-windows-environment/Modul09/TestKonta/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Konta;
+using System.IO;
+
+namespace TestKonta
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Konto k1 = new Konto("JanKowalski", "password");
+            Konto k2 = new Konto("AndrzejNowak", "haslo");
+
+            Konto.PrzedZmianaHasla +=
+                PolitykaBezpieczenstwa.SprawdzCzyHasloJestPowtorzone;
+            Konto.PrzedZmianaHasla +=
+                PolitykaBezpieczenstwa.SprawdzDlogoscHasla;
+
+            Konto.PoZmianieHasla += PolitykaBezpieczenstwa.ZapiszDoPliku;
+            
+            if (k1.ZmienHaslo("password", "123"))
+                Console.WriteLine("Hasło zmienione.");
+            else
+                Console.WriteLine("Hasła nie udało się zmienić.");
+            
+            if (k2.ZmienHaslo("haslo", "haslo"))
+                Console.WriteLine("Hasło zmienione.");
+            else
+                Console.WriteLine("Hasła nie udało się zmienić.");
+            
+            if (k1.ZmienHaslo("password", "12345"))
+                Console.WriteLine("Hasło zmienione.");
+            else
+                Console.WriteLine("Hasła nie udało się zmienić.");
+            
+            if (k2.ZmienHaslo("haslo", "123haslo"))
+                Console.WriteLine("Hasło zmienione.");
+            else
+                Console.WriteLine("Hasła nie udało się zmienić.");
+
+            Console.ReadKey();
+        }
+    }
+
+    static class PolitykaBezpieczenstwa
+    {
+        public static int MinimalnaLiczbaZnakow { set; get; }
+        public static string NazwaPliku { set; get; }
+
+        static PolitykaBezpieczenstwa()
+        {
+            MinimalnaLiczbaZnakow = 5;
+            NazwaPliku = "Audyt.txt";
+        }
+
+        public static void SprawdzDlogoscHasla(object sender,
+            PrzedZmianaHaslaArgs e)
+        {
+            if (e.NoweHaslo.Length < MinimalnaLiczbaZnakow)
+                e.Cancel = true;
+        }
+
+        public static void SprawdzCzyHasloJestPowtorzone(object sender,
+            PrzedZmianaHaslaArgs e)
+        {
+            if (e.NoweHaslo == e.StareHaslo)
+                e.Cancel = true;
+        }
+
+        public static void ZapiszDoPliku(object sender, EventArgs e)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(NazwaPliku, true);
+                Konto k = (Konto)sender;
+                sw.WriteLine("Zmiana hasła dla użytkownika {0}",
+                    k.NazwaUzytkownika);
+            }
+            finally
+            {
+                if (sw != null)
+                    sw.Close();
+            }
+        }
+    }
+}
