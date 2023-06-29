1a7703cb5984b6402cd5da0e4e8f0fc9d7b84ca2
mike <saintsikorski@gmail.com>
2023-06-29 22:56:39 +0200

    Add 'No hotel information' message
    Display the alert when no hotel objects are found in the database.

--- Sem IV/Advanced-object-oriented-programming/exam/hotel/src/main/java/com/example/hotel/ui/HomeView.java
+++ Sem IV/Advanced-object-oriented-programming/exam/hotel/src/main/java/com/example/hotel/ui/HomeView.java
@@ -2,6 +2,8 @@ package com.example.hotel.ui;
 
 import com.example.hotel.service.HotelServiceImpl;
 import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
@@ -20,13 +22,21 @@ public class HomeView extends VerticalLayout {
         super.onAttach(attachEvent);
         Map<String, Integer> totalRoomsByHotel = hotelService.getTotalRoomsByHotel();
 
-        for (Map.Entry<String, Integer> hotel : totalRoomsByHotel.entrySet()) {
-            String locationName = hotel.getKey();
-            Integer totalRooms = hotel.getValue();
-            TextField textField = new TextField("Hotel name: " + locationName);
-            textField.setValue("Total rooms: " + totalRooms);
-            textField.setReadOnly(true);
-            add(textField);
+        if (totalRoomsByHotel.isEmpty()) {
+            VerticalLayout errorContainer = new VerticalLayout();
+            H3 noHotelsFoundHeader = new H3("No hotel information");
+            Text noHotelsFoundMessage = new Text("No hotel objects were found in the database.");
+            errorContainer.add(noHotelsFoundHeader, noHotelsFoundMessage);
+            add(errorContainer);
+        } else {
+            for (Map.Entry<String, Integer> hotel : totalRoomsByHotel.entrySet()) {
+                String locationName = hotel.getKey();
+                Integer totalRooms = hotel.getValue();
+                TextField textField = new TextField("Hotel name: " + locationName);
+                textField.setValue("Total rooms: " + totalRooms);
+                textField.setReadOnly(true);
+                add(textField);
+            }
         }
     }
 
