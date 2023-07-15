b04b62fe9989ca84e43a7e5b5f4349a806cf6f2d
mike <saintsikorski@gmail.com>
2023-07-15 09:32:10 +0200

    Import clean inpost project

---
+++ Sem IV/Advanced-object-oriented-programming/exam/inpost/src/main/java/com/example/inpost/InpostApplication.java
@@ -0,0 +1,13 @@
+package com.example.inpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InpostApplication.class, args);
+	}
+
+}
---
+++ Sem IV/Advanced-object-oriented-programming/exam/inpost/src/test/java/com/example/inpost/InpostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
