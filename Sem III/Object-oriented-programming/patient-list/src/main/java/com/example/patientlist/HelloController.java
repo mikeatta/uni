f24e401255b4c17efbe23a976d51430c9434e74e
mike <saintsikorski@gmail.com>
2023-08-10 14:27:02 +0200

    Start new project

--- Sem III/Object-oriented-programming/patient-list/src/main/java/com/example/patientlist/HelloController.java
+++ Sem III/Object-oriented-programming/patient-list/src/main/java/com/example/patientlist/HelloController.java
@@ -1,29 +1,14 @@
 package com.example.patientlist;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 
-import java.util.ResourceBundle;
-import java.net.URL;
-
-public class HelloController implements Initializable {
-    public Label formName;
-    public Button printDataButton;
+public class HelloController {
     @FXML
-    private ChoiceBox<String> myChoiceBox;
-    
-    private String[] values = {"Patient1", "Patient2", "Patient3"};
-
-    @Override
-    public void initialize(URL arg0, ResourceBundle arg1) {
-        myChoiceBox.getItems().addAll(values);
-    }
+    private Label welcomeText;
 
     @FXML
-    protected void onPrintDataButton() {
-        System.out.println((myChoiceBox.getValue()));
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
