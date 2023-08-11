d27a0f419448ae7b73d169f0481f5fc4288bdfee
mike <saintsikorski@gmail.com>
2023-08-11 10:28:57 +0200

    Implement populating the ComboBox

--- Sem III/Object-oriented-programming/patient-list/src/main/java/com/example/patientlist/HelloController.java
+++ Sem III/Object-oriented-programming/patient-list/src/main/java/com/example/patientlist/HelloController.java
@@ -1,14 +1,35 @@
 package com.example.patientlist;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField patientName;
+    @FXML
+    private TextField patientSurname;
+    @FXML
+    private TextField patientAge;
+    @FXML
+    private ComboBox<String> sortTypeCombo;
+
+    @FXML
+    private void initialize() {
+        setUpComboBox();
     }
+
+    private void setUpComboBox() {
+        List<String> sortTypes = new ArrayList<>(4);
+        sortTypes.add("Name");
+        sortTypes.add("Surname");
+        sortTypes.add("Age");
+        sortTypes.add("Date added");
+        sortTypeCombo.getItems().addAll(sortTypes);
+    }
+
 }
