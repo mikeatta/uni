bbf397964a3609c01c0d26468c2bba764239fbad
mike <saintsikorski@gmail.com>
2023-08-12 14:56:11 +0200

    Get selected ComboBox option

--- Sem III/Object-oriented-programming/patient-list/src/main/java/com/example/patientlist/HelloController.java
+++ Sem III/Object-oriented-programming/patient-list/src/main/java/com/example/patientlist/HelloController.java
@@ -1,10 +1,12 @@
 package com.example.patientlist;
 
+import com.example.patientlist.model.Patient;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class HelloController {
@@ -18,6 +20,8 @@ public class HelloController {
     @FXML
     private ComboBox<String> sortTypeCombo;
 
+    private List<Patient> patients = new LinkedList<>();
+
     @FXML
     private void initialize() {
         setUpComboBox();
@@ -30,6 +34,22 @@ public class HelloController {
         sortTypes.add("Age");
         sortTypes.add("Date added");
         sortTypeCombo.getItems().addAll(sortTypes);
+    }
+
+    private String getComboBoxValue() {
+        return String.valueOf(sortTypeCombo.getValue());
+    }
+
+    @FXML
+    private void displayPatientInfo() {
+        String comboValue = getComboBoxValue();
+        switch (comboValue) {
+            case "Name" -> System.out.println("Selected 'Name'");
+            case "Surname" -> System.out.println("Selected 'Surname'");
+            case "Age" -> System.out.println("Selected 'Age'");
+            case "Date added" -> System.out.println("Selected 'Date added'");
+            default -> System.err.println("Please select order type!");
+        }
     }
 
 }
