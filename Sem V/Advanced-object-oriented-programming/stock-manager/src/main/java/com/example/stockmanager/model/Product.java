86acade22dea51a5c1bfdc8dea7b60679ed03dd6
mike <saintsikorski@gmail.com>
2023-12-03 21:54:03 +0100

    Add Product.java

---
+++ Sem V/Advanced-object-oriented-programming/stock-manager/src/main/java/com/example/stockmanager/model/Product.java
@@ -0,0 +1,70 @@
+package com.example.stockmanager.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String size;
+
+    @Column
+    private String sku;
+
+    @Column
+    private BigDecimal purchasePrice;
+
+    @Column
+    private BigDecimal marketPrice;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public BigDecimal getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(BigDecimal purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public BigDecimal getMarketPrice() {
+        return marketPrice;
+    }
+
+    public void setMarketPrice(BigDecimal marketPrice) {
+        this.marketPrice = marketPrice;
+    }
+}
