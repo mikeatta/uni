37450ce82d1bec00e391736c1d753c40715c74f2
mike <saintsikorski@gmail.com>
2023-12-03 22:04:52 +0100

    Annotate Product.java
    Add missing Spring Boot entity annotation to the Product.java class.

--- Sem V/Advanced-object-oriented-programming/stock-manager/src/main/java/com/example/stockmanager/model/Product.java
+++ Sem V/Advanced-object-oriented-programming/stock-manager/src/main/java/com/example/stockmanager/model/Product.java
@@ -1,12 +1,10 @@
 package com.example.stockmanager.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
+@Entity
 public class Product {
 
     @Id
