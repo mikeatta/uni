ff533d9769e358295bb9f584c0ef1e6e14a51976
mike <saintsikorski@gmail.com>
2023-12-10 23:08:01 +0100

    Add Products.vue
    Add component for listing products from the database.

---
+++ Sem V/Advanced-object-oriented-programming/stock-manager/src/frontend/src/components/Products.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const product = ref('');
+
+onMounted(async () => {
+  const response = await fetch('/api/v1/products');
+  product.value = await response.text();
+})
+</script>
+
+<template>
+  <h1>List of products:</h1>
+  <p>{{ product }}</p>
+</template>
+
+<style scoped>
+
+</style>
