b8253b71c609745d05c48aabb235caf1cd4e32af
mike <saintsikorski@gmail.com>
2023-12-17 22:56:51 +0100

    Display products in a table
    Create a table to showcase products fetched from the API request.

--- Sem V/Advanced-object-oriented-programming/stock-manager/src/frontend/src/components/Products.vue
+++ Sem V/Advanced-object-oriented-programming/stock-manager/src/frontend/src/components/Products.vue
@@ -1,17 +1,49 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted } from 'vue';
 
-const product = ref('');
+const products = ref([]);
 
 onMounted(async () => {
-  const response = await fetch('/api/v1/products');
-  product.value = await response.text();
-})
+  try {
+    const response = await fetch('/api/v1/products');
+    const data = await response.json();
+    products.value = data.map(product => ({
+      name: product.name || '',
+      size: product.size || '',
+      sku: product.sku || '',
+      purchasePrice: product.purchasePrice || 0,
+      marketPrice: product.marketPrice || 0,
+    }));
+  } catch (error) {
+    console.error('Error fetching products:', error);
+  }
+});
 </script>
 
 <template>
-  <h1>List of products:</h1>
-  <p>{{ product }}</p>
+  <div>
+    <h1>List of products:</h1>
+    <table>
+      <thead>
+      <tr>
+        <th>Name</th>
+        <th>Size</th>
+        <th>SKU</th>
+        <th>Purchase price</th>
+        <th>Market price</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for='product in products' :key='product.id'>
+        <td>{{ product.name }}</td>
+        <td>{{ product.size }}</td>
+        <td>{{ product.sku }}</td>
+        <td>{{ product.purchasePrice }}</td>
+        <td>{{ product.marketPrice }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <style scoped>
