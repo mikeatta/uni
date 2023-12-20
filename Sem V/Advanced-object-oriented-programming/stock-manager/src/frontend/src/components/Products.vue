0e123f184d06928e3a51b08bea4d4392deb30fdb
mike <saintsikorski@gmail.com>
2023-12-20 23:56:59 +0100

    Implement adding new products
    Create a form for entering new product data and add new products to the
    database upon submission.
    
    Change the default value for purchasePrice and marketPrice fields from
    a single digit (0) to a floating point number (0.00).

--- Sem V/Advanced-object-oriented-programming/stock-manager/src/frontend/src/components/Products.vue
+++ Sem V/Advanced-object-oriented-programming/stock-manager/src/frontend/src/components/Products.vue
@@ -2,6 +2,13 @@
 import { ref, onMounted } from 'vue';
 
 const products = ref([]);
+const newProduct = ref({
+  name: '',
+  size: '',
+  sku: '',
+  purchasePrice: 0,
+  marketPrice: 0,
+});
 
 onMounted(async () => {
   try {
@@ -11,18 +18,61 @@ onMounted(async () => {
       name: product.name || '',
       size: product.size || '',
       sku: product.sku || '',
-      purchasePrice: product.purchasePrice || 0,
-      marketPrice: product.marketPrice || 0,
+      purchasePrice: product.purchasePrice || 0.00,
+      marketPrice: product.marketPrice || 0.00,
     }));
   } catch (error) {
     console.error('Error fetching products:', error);
   }
 });
+
+async function addProduct() {
+  try {
+    const addResponse = await fetch('/api/v1/products', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newProduct.value)
+    });
+
+    if (addResponse.ok) {
+      // Update the product list
+      const addedProduct = await addResponse.json();
+      products.value = [...products.value, addedProduct];
+
+      // Clear the form
+      newProduct.value = {
+        name: '',
+        size: '',
+        sku: '',
+        purchasePrice: 0,
+        marketPrice: 0,
+      };
+    } else {
+      console.error('Error adding product:', addResponse.status, addResponse.statusText);
+    }
+  } catch (error) {
+    console.log('Error adding product:', error);
+  }
+}
+
 </script>
 
 <template>
   <div>
     <h1>List of products:</h1>
+
+    <!-- Form for adding new products -->
+    <form @submit.prevent='addProduct'>
+      <label>Name: <input v-model='newProduct.name' required /></label><br>
+      <label>Size: <input v-model='newProduct.size' required /></label><br>
+      <label>SKU: <input v-model='newProduct.sku' required /></label><br>
+      <label>Purchase price: <input v-model.number='newProduct.purchasePrice' required /></label><br>
+      <label>Market price: <input v-model.number='newProduct.marketPrice' required /></label><br>
+      <button type='submit'>Add Product</button>
+    </form>
+
     <table>
       <thead>
       <tr>
