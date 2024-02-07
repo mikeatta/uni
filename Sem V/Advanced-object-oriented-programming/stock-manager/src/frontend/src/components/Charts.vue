30f6e167aa06002bdb02ee396ed5a767b251a4dd
mike <saintsikorski@gmail.com>
2024-02-07 22:16:35 +0100

    Add category statistic charts in Charts.vue
    Add charts with total product counts and total market price values,
    grouped by their categories.

--- Sem V/Advanced-object-oriented-programming/stock-manager/src/frontend/src/components/Charts.vue
+++ Sem V/Advanced-object-oriented-programming/stock-manager/src/frontend/src/components/Charts.vue
@@ -3,18 +3,39 @@ import { onMounted, watch } from 'vue';
 import Chart from 'chart.js/auto';
 
 const props = defineProps(['products']);
-let productChart;
+let categoryCountChart;
+let categoryValueChart;
 
-function generateChart() {
-  const data = {
+let categories = [];
+let counts = [];
+let values = [];
+
+function getCategoryStatistics() {
+  const categoryCounts = {};
+  const categoryValues = {};
+
+  props.products.forEach(product => {
+    const category = product.category;
+    const marketPrice = product.marketPrice;
+    categoryCounts[category] = (categoryCounts[category] || 0) + 1;
+    categoryValues[category] = (categoryValues[category] || 0) + marketPrice;
+  });
+
+  categories = Object.keys(categoryCounts);
+  counts = categories.map(category => categoryCounts[category]);
+  values = categories.map(category => categoryValues[category]);
+}
+
+function generateCharts() {
+  const categoryCountData = {
     type: 'doughnut',
     data: {
-      labels: props.products.map(product => product.name),
+      labels: categories,
       datasets: [{
-        label: 'Value',
-        data: props.products.map(product => product.marketPrice),
-        backgroundColor: "rgba(54,73,93,.5)",
-        borderColor: "#36495d",
+        label: 'Count',
+        data: counts,
+        backgroundColor: 'rgba(54, 73, 93, .5)',
+        borderColor: '#36495d',
         borderWidth: 3
       }]
     },
@@ -28,31 +49,65 @@ function generateChart() {
     }
   };
 
-  const ctx = document.getElementById('categoryValueChart');
-  productChart = new Chart(ctx, data);
+  const categoryValueData = {
+    type: 'doughnut',
+    data: {
+      labels: categories,
+      datasets: [{
+        label: 'Value',
+        data: values,
+        backgroundColor: 'rgba(54, 73,93, .5)',
+        borderColor: '#36495d',
+        borderWidth: 3
+      }]
+    },
+    options: {
+      responsive: false,
+      plugins: {
+        legend: {
+          display: true
+        }
+      }
+    }
+  };
+
+  const ctxCount = document.getElementById('categoryCountChart');
+  const ctxValue = document.getElementById('categoryValueChart');
+
+  categoryCountChart = new Chart(ctxCount, categoryCountData);
+  categoryValueChart = new Chart(ctxValue, categoryValueData);
 }
 
-function updateChart() {
-  productChart.data.labels = props.products.map(product => product.name);
-  productChart.data.datasets[0].data = props.products.map(product => product.marketPrice);
-  productChart.update();
+function updateCharts() {
+  categoryCountChart.data.labels = categories;
+  categoryCountChart.data.datasets[0].data = counts;
+
+  categoryValueChart.data.labels = categories;
+  categoryValueChart.data.datasets[0].data = values;
+
+  categoryCountChart.update();
+  categoryValueChart.update();
 }
 
 onMounted(() => {
-  generateChart();
+  generateCharts();
 });
 
 watch(() => props.products, (newProducts, oldProducts) => {
-  updateChart();
+  getCategoryStatistics();
+  updateCharts();
 }, {deep: true});
 </script>
 
 <template>
-  <div>
-    <canvas id="categoryValueChart" width="320" height="320"></canvas>
+  <div class='productCharts'>
+    <canvas id='categoryCountChart' width='320' height='320'></canvas>
+    <canvas id='categoryValueChart' width='320' height='320'></canvas>
   </div>
 </template>
 
 <style scoped>
-
+.productCharts {
+  display: flex;
+}
 </style>
