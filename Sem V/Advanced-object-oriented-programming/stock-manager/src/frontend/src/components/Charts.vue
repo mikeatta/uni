0f35feb061d87b8e402ae4f7928bd1b85612546b
mike <saintsikorski@gmail.com>
2024-02-07 14:21:15 +0100

    Display a chart in the Charts.vue component
    Fix the chart rendering issue. Properly display the chart and labels.

--- Sem V/Advanced-object-oriented-programming/stock-manager/src/frontend/src/components/Charts.vue
+++ Sem V/Advanced-object-oriented-programming/stock-manager/src/frontend/src/components/Charts.vue
@@ -1,77 +1,56 @@
 <script setup>
-import {onMounted, ref, watch} from 'vue';
+import { onMounted, watch } from 'vue';
 import Chart from 'chart.js/auto';
 
 const props = defineProps(['products']);
-const chartUpdated = ref(false);
 let productChart;
 
-const data = {
-  type: 'doughnut',
-  data: {
-    labels: props.products.map(product => product.name),
-    datasets: [
-      {
+function generateChart() {
+  const data = {
+    type: 'doughnut',
+    data: {
+      labels: props.products.map(product => product.name),
+      datasets: [{
         label: 'Value',
         data: props.products.map(product => product.marketPrice),
         backgroundColor: "rgba(54,73,93,.5)",
         borderColor: "#36495d",
         borderWidth: 3
+      }]
+    },
+    options: {
+      responsive: false,
+      plugins: {
+        legend: {
+          display: true
+        }
       }
-    ]
-  },
-  options: {
-    responsive: false,
-    legend: {
-      display: false
     }
-  }
-};
-
-function generateChart() {
-  if (productChart) {
-    productChart.destroy();
-    console.log('Destroyed old chart');
-  }
-
-  document.querySelector(".chartContainer").innerHTML =
-      '<canvas id="categoryValueChart" v-if="chartUpdated"></canvas>';
+  };
 
   const ctx = document.getElementById('categoryValueChart');
   productChart = new Chart(ctx, data);
-  productChart.render();
-  console.log('Chart generated');
 }
 
 function updateChart() {
-  chartUpdated.value = true;
-
-  if (productChart) {
-    productChart.destroy();
-    console.log('Destroyed old chart');
-  }
-
-  document.querySelector(".chartContainer").innerHTML =
-      '<canvas id="categoryValueChart" v-if="chartUpdated"></canvas>';
-
   productChart.data.labels = props.products.map(product => product.name);
-  productChart.data.datasets.data = props.products.map(product => product.marketPrice);
-  generateChart();
-  productChart.render();
+  productChart.data.datasets[0].data = props.products.map(product => product.marketPrice);
+  productChart.update();
 }
 
 onMounted(() => {
   generateChart();
-})
+});
 
 watch(() => props.products, (newProducts, oldProducts) => {
   updateChart();
-  console.log('Products updated:', newProducts);
 }, {deep: true});
 </script>
 
 <template>
-  <div class='chartContainer'></div>
+  <div>
+    <canvas id="categoryValueChart" width="320" height="320"></canvas>
+  </div>
 </template>
 
 <style scoped>
