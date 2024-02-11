1485cd0c8890d7b4cc497b19e6958200dcf289bb
mike <saintsikorski@gmail.com>
2024-02-11 15:49:30 +0100

    Change the summary item positioning in App.vue
    Place the product statistic elements alongside the charts.

--- Sem V/Advanced-object-oriented-programming/stock-manager/src/frontend/src/App.vue
+++ Sem V/Advanced-object-oriented-programming/stock-manager/src/frontend/src/App.vue
@@ -13,12 +13,27 @@ const updateProducts = (updatedProducts) => {
 
 <template>
   <main>
-    <Charts :products='products' />
-    <Summary :products='products' />
+    <div class='statisticalElements'>
+      <Charts :products='products' class='charts' />
+      <Summary :products='products' class='summary' />
+    </div>
     <Products @updateProductList='updateProducts' />
   </main>
 </template>
 
 <style scoped>
+.statisticalElements {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 5%;
+}
 
+.charts {
+  flex: 3;
+}
+
+.summary {
+  flex: 1;
+}
 </style>
