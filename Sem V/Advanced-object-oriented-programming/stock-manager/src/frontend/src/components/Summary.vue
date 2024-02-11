954046ca3bd2854f74c07b53f7c72c6327a75401
mike <saintsikorski@gmail.com>
2024-02-11 15:45:15 +0100

    Add summary item styling in Summary.vue
    Add borders and hover effects to the statistical elements.
    
    Change item positioning to vertical.

--- Sem V/Advanced-object-oriented-programming/stock-manager/src/frontend/src/components/Summary.vue
+++ Sem V/Advanced-object-oriented-programming/stock-manager/src/frontend/src/components/Summary.vue
@@ -14,7 +14,7 @@ const { products } = defineProps(['products']);
         <p>$ {{ products.reduce((sum, product) => sum + product.marketPrice, 0).toFixed(2) }}</p>
       </div>
       <div id='totalProductValue' class='summaryItem'>
-        <h3>Total profit</h3>
+        <h3>Total Profit</h3>
         <p>$ {{ products.reduce((sum, product) => sum + product.amountMade, 0).toFixed(2) }}</p>
       </div>
     </div>
@@ -28,7 +28,26 @@ const { products } = defineProps(['products']);
 
 .summaryContainer {
   display: flex;
-  flex-direction: row;
-  width: 90%;
+  flex-direction: column;
+  justify-content: space-evenly;
+  width: 100%;
+  height: 100%;
+}
+
+.summaryItem {
+  appearance: none;
+  -webkit-appearance: none;
+  font-size: 0.8rem;
+  padding: 0.6rem;
+  border: 0.1rem solid white;
+  border-radius: 0.5rem;
+  background-color: rgba(18, 18, 18, 0.33);
+  color: white;
+}
+
+.summaryItem:hover {
+  background-color: #494848;
+  color: white;
+  transition-duration: 300ms;
 }
 </style>
