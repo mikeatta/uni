b0a5a38a96bba6ecaeafa1ca2991b2a316f7c273
mike <saintsikorski@gmail.com>
2024-02-16 22:30:41 +0100

    Display calendar data in App.js
    List the event, tasklist and task information received from the API
    request on the fronend of the application.

--- Sem V/Mobile-applications-programming/calendar-app/frontend/App.js
+++ Sem V/Mobile-applications-programming/calendar-app/frontend/App.js
@@ -1,11 +1,87 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+  const [calendarData, setCalendarData] = useState({
+    events: [],
+    tasklists: [],
+    tasks: [],
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'http://localhost:3000/api/v1/calendar'
+        );
+        setCalendarData(response.data);
+      } catch (error) {
+        console.error('Error fetching calendar data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Calendar Data</Text>
+
+      <Text>Events:</Text>
+      {calendarData.events.length === 0 ? (
+        <Text>No events found.</Text>
+      ) : (
+        <View>
+          {calendarData.events.map((event, index) => {
+            const start = event.start.dateTime || event.start.date;
+            <Text key={index}>
+              {start} - {event.summary}
+            </Text>;
+          })}
+        </View>
+      )}
+
+      <Text>Tasklists:</Text>
+      {calendarData.tasklists.length === 0 ? (
+        <Text>No tasklists found.</Text>
+      ) : (
+        <View>
+          {calendarData.tasklists.map((tasklist, index) => {
+            const title = tasklist.title;
+            const id = tasklist.id;
+
+            return (
+              <Text key={index}>
+                {index + 1}. {title} - ID: {id}
+              </Text>
+            );
+          })}
+        </View>
+      )}
+
+      <Text>Tasks:</Text>
+      {calendarData.tasks.length === 0 ? (
+        <Text>No tasks found.</Text>
+      ) : (
+        <View>
+          {calendarData.tasks.map((task, index) => {
+            const due = new Date(task.due).toISOString().split('T')[0];
+            const title = task.title;
+            const notes = task.notes !== undefined ? task.notes : '';
+
+            // Conditional rendering based on the presence of notes
+            return (
+              <Text key={index}>
+                {notes ? `${due} - ${title}: ${notes}` : `${due} - ${title}`}
+              </Text>
+            );
+          })}
+        </View>
+      )}
+
+      <StatusBar style='auto' />
     </View>
   );
 }
