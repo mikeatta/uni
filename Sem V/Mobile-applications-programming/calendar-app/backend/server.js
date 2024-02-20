6d74ddd81bd8ce89c4fc5628bec5aef0d06c0fd8
mike <saintsikorski@gmail.com>
2024-02-20 22:30:00 +0100

    Add a POST endpoint for creating new entries
    Parse the request body data and handle the creation of the
    corresponding entry type with the backend functions.

--- Sem V/Mobile-applications-programming/calendar-app/backend/server.js
+++ Sem V/Mobile-applications-programming/calendar-app/backend/server.js
@@ -3,13 +3,25 @@
 import express from 'express';
 import cors from 'cors';
 import { authenticateAndGetClient } from './auth.js';
-import { listEvents, listTasklists, listTasks } from './taskManager.js';
+import {
+  listEvents,
+  addEvent,
+  listTasklists,
+  listTasks,
+  addTask,
+} from './taskManager.js';
 
 const app = express();
 const port = 3000;
 
 // Enable CORS for all routes
 app.use(cors());
+
+// Parse JSON
+app.use(express.json());
+
+// Parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/api/v1/calendar', async (req, res) => {
   try {
@@ -23,6 +35,43 @@ app.get('/api/v1/calendar', async (req, res) => {
     res.json({ events, tasklists, tasks });
   } catch (err) {
     console.error('Error:', err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.post('/api/v1/calendar/new-entry', async (req, res) => {
+  try {
+    const { title, description, dateTime, type } = req.body;
+    const [calendar, service] = await authenticateAndGetClient();
+
+    if (type === 'event') {
+      const event = {
+        summary: title,
+        description: description,
+        start: {
+          dateTime: dateTime,
+          timeZone: 'CET',
+        },
+        end: {
+          dateTime: dateTime,
+          timeZone: 'CET',
+        },
+      };
+
+      await addEvent(calendar, event);
+    } else {
+      const task = {
+        title: title,
+        notes: description,
+        due: dateTime,
+      };
+
+      await addTask(service, task);
+    }
+
+    res.status(200).json({ message: 'Event added successfully!' });
+  } catch (err) {
+    console.error('Error', err);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
