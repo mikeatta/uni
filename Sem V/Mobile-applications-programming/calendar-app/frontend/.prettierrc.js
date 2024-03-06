0444e1b947d551d3d24c41aa75b58a7fcf137eb5
Mike Atta <saintsikorski@gmail.com>
2024-03-06 16:02:20 +0100

    Update prettier config

--- Sem V/Mobile-applications-programming/calendar-app/frontend/.prettierrc.js
+++ Sem V/Mobile-applications-programming/calendar-app/frontend/.prettierrc.js
@@ -1,7 +1,25 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
   singleQuote: true,
+  arrowParens: 'always',
+  semi: false,
   trailingComma: 'all',
-};
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  overrides: [
+    {
+      files: '*.tsx',
+      options: {
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        jsxSingleQuote: true,
+        jsxBracketSameLine: false,
+        arrowParens: 'always',
+        singleQuote: true,
+        bracketSpacing: true,
+        trailingComma: 'all',
+      },
+    },
+  ],
+}
