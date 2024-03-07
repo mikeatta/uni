62e2e1b585e5f4800781fa7a9d66319013969c78
Mike Atta <saintsikorski@gmail.com>
2024-03-07 22:34:47 +0100

    Add ListView.tsx
    Implement rendering the event and task properties inside the view.

---
+++ Sem V/Mobile-applications-programming/calendar-app/frontend/components/views/ListView.tsx
@@ -0,0 +1,19 @@
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Events from '../common/Events';
+import CalendarTasks from '../common/Tasks';
+import { CalendarData } from '../types';
+
+export default function ListView({ events, tasks }: CalendarData) {
+  return (
+    <View>
+      <Text>Calendar Data</Text>
+      <ScrollView>
+        <Events events={events} />
+        <CalendarTasks tasks={tasks} />
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
