aad0e427aca75fbb44788dcb2d575f8498500920
Mike Atta <saintsikorski@gmail.com>
2024-03-09 14:54:53 +0100

    Add DateTimePicker.tsx
    Introduce the component to handle date and time selection when creating
    new calendar entries.

---
+++ Sem V/Mobile-applications-programming/calendar-app/frontend/components/common/DateTimePicker.tsx
@@ -0,0 +1,37 @@
+import { Button, StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import DatePicker from 'react-native-date-picker';
+import { DateTimeSelector } from '../types';
+
+function DateTimePicker({
+  title,
+  dateTime,
+  dateTimeType,
+  setDateTime,
+}: DateTimeSelector) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <View>
+      <Button title='Open' onPress={() => setOpen(true)} />
+      <DatePicker
+        modal
+        title={title}
+        open={open}
+        date={dateTime}
+        onConfirm={(date) => {
+          setOpen(false);
+          setDateTime((prev) => ({
+            ...prev,
+            [dateTimeType]: { dateTime: date },
+          }));
+        }}
+        onCancel={() => setOpen(false)}
+      />
+    </View>
+  );
+}
+
+export default DateTimePicker;
+
+const styles = StyleSheet.create({});
