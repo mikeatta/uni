65bec3584a4a63da775c7e6a5e9223f53c809885
Mike Atta <saintsikorski@gmail.com>
2024-03-10 14:57:18 +0100

    Add DateOnlyPicker.tsx
    Implement a picker component for selecting only the date value.
    
    The component will be used for task entries, which do not allow
    altering the time via the Google Tasks API.

---
+++ Sem V/Mobile-applications-programming/calendar-app/frontend/components/common/DateOnlyPicker.tsx
@@ -0,0 +1,32 @@
+import { Button, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import DatePicker from 'react-native-date-picker';
+import { DateTimeSelector } from '../types';
+
+function DateOnlyPicker({ title, dateTime, setDateTime }: DateTimeSelector) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <View>
+      <Button title='Open' onPress={() => setOpen(true)} />
+      <DatePicker
+        modal
+        title={title}
+        open={open}
+        mode='date'
+        date={dateTime}
+        onConfirm={(date) => {
+          setOpen(false);
+          setDateTime((prev) => ({
+            ...prev,
+            start: { ...prev.start, dateTime: date },
+          }));
+        }}
+      />
+    </View>
+  );
+}
+
+export default DateOnlyPicker;
+
+const styles = StyleSheet.create({});
