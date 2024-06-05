9e0e3f1b9458daf1e29505eb52290fc099acfefd
Mike Atta <saintsikorski@gmail.com>
2024-06-05 21:32:57 +0200

    Update ESLint config
    Disable some of React Native's default linter warnings.
    
    This commit removes the linter warnings for unavoidable elements in the
    code structure.

--- Sem V/Mobile-applications-programming/calendar-app/frontend/.eslintrc.js
+++ Sem V/Mobile-applications-programming/calendar-app/frontend/.eslintrc.js
@@ -3,5 +3,13 @@ module.exports = {
   extends: '@react-native',
   rules: {
     semi: 0,
+    'react-native/no-inline-styles': 0,
+    'react-hooks/exhaustive-deps': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        'no-inline-styles': false,
+      },
+    ],
   },
 }
