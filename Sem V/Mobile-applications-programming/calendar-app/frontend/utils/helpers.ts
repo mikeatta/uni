e70fb9a0402e1b36d38d073fc863eed981394141
Mike Atta <saintsikorski@gmail.com>
2024-06-08 21:29:40 +0200

    Add 'getDataType()'
    This commit adds the helper function to 'helpers.ts'. The function is
    used for returning the calendar entry type.

--- Sem V/Mobile-applications-programming/calendar-app/frontend/utils/helpers.ts
+++ Sem V/Mobile-applications-programming/calendar-app/frontend/utils/helpers.ts
@@ -1,3 +1,5 @@
+import { CalendarEvent, CalendarTask, FormData } from '../components/types'
+
 /*
  * Function: 'withFunction'
  *
@@ -13,4 +15,31 @@ export const withFunction = <T extends (...args: any[]) => any>(
       await finalFunction()
     }
   }
+}
+
+/*
+ * Function: 'getDataType'
+ *
+ * A function returning the calendar entry data type.
+ */
+export const getDataType = (entry: CalendarEvent | CalendarTask) => {
+  if (isCalendarEvent(entry)) {
+    return 'event'
+  } else if (isCalendarTask(entry)) {
+    return 'task'
+  } else {
+    throw Error('Unknown type of submitted entry')
+  }
+}
+
+function isCalendarEvent(
+  entry: CalendarEvent | CalendarTask,
+): entry is CalendarEvent {
+  return (entry as CalendarEvent).summary !== undefined
+}
+
+function isCalendarTask(
+  entry: CalendarEvent | CalendarTask,
+): entry is CalendarTask {
+  return (entry as CalendarTask).title !== undefined
 }
