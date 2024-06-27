e8be181a5503053896275166df788257552ace77
Mike Atta <saintsikorski@gmail.com>
2024-06-27 22:11:47 +0200

    Refactor syncing local data with Google Calendar
    Add the 'calendarUpdatesNeeded()' function to verify the need for
    updating the local database.
    
    Remove unused parameters from the hook.
    
    Rename variables and types to more descriptive names.
    
    This commit syncs the local database to the state of the Google
    Calendar on the first run and after reestablishing the connection.

--- Sem V/Mobile-applications-programming/calendar-app/frontend/hooks/useSyncChanges.ts
+++ Sem V/Mobile-applications-programming/calendar-app/frontend/hooks/useSyncChanges.ts
@@ -3,17 +3,19 @@ import { CalendarEvent, CalendarTask, ICalendarData } from '../components/types'
 import { connectToDatabase } from '../db/db'
 import { addEvent, removeEvent } from '../db/events'
 import { addTask, removeTask } from '../db/tasks'
+import { fetchGoogleCalendarData } from '../services/api/googleCalendar'
 
 export const useSyncChanges = async (
   localData: ICalendarData,
-  calendarData: ICalendarData,
-  isDatabaseSetup: boolean,
-  refetchLocalData: () => Promise<void>,
+  isConnected: boolean,
+  refreshLocalEntryList: () => Promise<void>,
 ) => {
-  const [isLocalDatabaseSynced, setIsLocalDatabaseSynced] =
-    useState<boolean>(true)
+  const [lastSyncedLocalData, setLastSyncedLocalData] =
+    useState<ICalendarData>(localData)
 
-  type LocalDatabaseRef = {
+  const localDataNeedsUpdates = useRef<boolean>(false)
+
+  type DataStatus = {
     eventsToAdd: CalendarEvent[]
     eventsToRemove: CalendarEvent[]
     eventsToUpdate: CalendarEvent[]
@@ -22,129 +24,149 @@ export const useSyncChanges = async (
     tasksToUpdate: CalendarTask[]
   }
 
-  const localDatabaseChanges = useRef<LocalDatabaseRef>({
-    eventsToAdd: [],
-    eventsToRemove: [],
-    eventsToUpdate: [],
-    tasksToAdd: [],
-    tasksToRemove: [],
-    tasksToUpdate: [],
-  })
+  const compareCalendarData = useCallback(
+    async (newObj: ICalendarData, prevObj: ICalendarData) => {
+      const eventsToAdd = newObj.events.filter(
+        (newEvent) =>
+          !prevObj.events.some((prevEvent) => newEvent.id === prevEvent.id),
+      )
 
-  const getLocalDatabaseStatus = useCallback(async () => {
-    localDatabaseChanges.current.eventsToAdd = calendarData.events.filter(
-      (remoteEvent) =>
-        !localData.events.some(
-          (localEvent) => localEvent.id === remoteEvent.id,
-        ),
-    )
+      const eventsToRemove = prevObj.events.filter(
+        (prevEvent) =>
+          !newObj.events.some((newEvent) => newEvent.id === prevEvent.id),
+      )
 
-    localDatabaseChanges.current.eventsToRemove = localData.events.filter(
-      (localEvent) =>
-        !calendarData.events.some(
-          (remoteEvent) => localEvent.id === remoteEvent.id,
-        ),
-    )
-
-    localDatabaseChanges.current.eventsToUpdate = calendarData.events.filter(
-      (remoteEvent) =>
-        localData.events.some((localEvent) => {
+      const eventsToUpdate = newObj.events.filter((newEvent) =>
+        prevObj.events.some((prevEvent) => {
           return (
-            localEvent.id === remoteEvent.id &&
-            (localEvent.summary !== remoteEvent.summary ||
-              localEvent.description !== remoteEvent.description ||
-              localEvent.start.dateTime !== remoteEvent.start.dateTime ||
-              localEvent.start.timeZone !== remoteEvent.end.timeZone ||
-              localEvent.end.dateTime !== remoteEvent.end.dateTime ||
-              localEvent.end.timeZone !== remoteEvent.end.timeZone)
+            newEvent.id === prevEvent.id &&
+            (newEvent.summary !== prevEvent.summary ||
+              newEvent.description !== prevEvent.description ||
+              newEvent.start.dateTime !== prevEvent.start.dateTime ||
+              newEvent.start.timeZone !== prevEvent.end.timeZone ||
+              newEvent.end.dateTime !== prevEvent.end.dateTime ||
+              newEvent.end.timeZone !== prevEvent.end.timeZone)
           )
         }),
-    )
+      )
 
-    localDatabaseChanges.current.tasksToAdd = calendarData.tasks.filter(
-      (remoteTask) =>
-        !localData.tasks.some((localTask) => localTask.id === remoteTask.id),
-    )
+      const tasksToAdd = newObj.tasks.filter(
+        (newTask) =>
+          !prevObj.tasks.some((prevTask) => newTask.id === prevTask.id),
+      )
 
-    localDatabaseChanges.current.tasksToRemove = localData.tasks.filter(
-      (localTask) =>
-        !calendarData.tasks.some(
-          (remoteTask) => localTask.id === remoteTask.id,
-        ),
-    )
+      const tasksToRemove = prevObj.tasks.filter(
+        (prevTask) =>
+          !newObj.tasks.some((newTask) => newTask.id === prevTask.id),
+      )
 
-    localDatabaseChanges.current.tasksToUpdate = calendarData.tasks.filter(
-      (remoteTask) =>
-        localData.tasks.some((localTask) => {
+      const tasksToUpdate = newObj.tasks.filter((newTask) =>
+        prevObj.tasks.some((prevTask) => {
           return (
-            localTask.id === remoteTask.id &&
-            (localTask.title !== remoteTask.title ||
-              localTask.notes !== remoteTask.notes ||
-              localTask.status !== remoteTask.status ||
-              localTask.due !== remoteTask.due)
+            newTask.id === prevTask.id &&
+            (newTask.title !== prevTask.title ||
+              newTask.notes !== prevTask.notes ||
+              newTask.due !== prevTask.due ||
+              newTask.status !== prevTask.status)
           )
         }),
-    )
+      )
 
-    return (
-      localDatabaseChanges.current.eventsToAdd.length === 0 &&
-      localDatabaseChanges.current.eventsToRemove.length === 0 &&
-      localDatabaseChanges.current.eventsToUpdate.length === 0 &&
-      localDatabaseChanges.current.tasksToAdd.length === 0 &&
-      localDatabaseChanges.current.tasksToRemove.length === 0 &&
-      localDatabaseChanges.current.tasksToUpdate.length === 0
-    )
-  }, [calendarData])
+      return {
+        eventsToAdd,
+        eventsToRemove,
+        eventsToUpdate,
+        tasksToAdd,
+        tasksToRemove,
+        tasksToUpdate,
+      }
+    },
+    [localData],
+  )
 
-  const updateLocalDatabase = useCallback(async () => {
-    const db = await connectToDatabase()
+  const calendarUpdatesNeeded = useCallback(
+    (calendarChanges: DataStatus) => {
+      const allChanges = Object.values(calendarChanges).flat()
+      const needsUpdates = allChanges.length > 0
+      localDataNeedsUpdates.current = needsUpdates
+      return needsUpdates
+    },
+    [compareCalendarData],
+  )
 
-    const eventsToAddOrUpdate = [
-      ...localDatabaseChanges.current.eventsToAdd,
-      ...localDatabaseChanges.current.eventsToUpdate,
-    ]
+  const updateLocalDatabase = useCallback(
+    async ({
+      eventsToAdd,
+      eventsToRemove,
+      eventsToUpdate,
+      tasksToAdd,
+      tasksToRemove,
+      tasksToUpdate,
+    }: DataStatus) => {
+      const db = await connectToDatabase()
 
-    const tasksToAddOrUpdate = [
-      ...localDatabaseChanges.current.tasksToAdd,
-      ...localDatabaseChanges.current.tasksToUpdate,
-    ]
+      const eventsToAddOrUpdate = [...eventsToAdd, ...eventsToUpdate]
+      const tasksToAddOrUpdate = [...tasksToAdd, ...tasksToUpdate]
 
-    for (const event of eventsToAddOrUpdate) {
-      await addEvent(db, event)
+      for (const event of eventsToAddOrUpdate) {
+        await addEvent(db, event)
+      }
+
+      for (const event of eventsToRemove) {
+        await removeEvent(db, event)
+      }
+
+      for (const task of tasksToAddOrUpdate) {
+        await addTask(db, task)
+      }
+
+      for (const task of tasksToRemove) {
+        await removeTask(db, task)
+      }
+    },
+    [calendarUpdatesNeeded],
+  )
+
+  const updateLocalData = useCallback(async () => {
+    if (localDataNeedsUpdates.current === true) {
+      await refreshLocalEntryList()
     }
-
-    for (const event of localDatabaseChanges.current.eventsToRemove) {
-      await removeEvent(db, event)
-    }
-
-    for (const task of tasksToAddOrUpdate) {
-      await addTask(db, task)
-    }
-
-    for (const task of localDatabaseChanges.current.tasksToRemove) {
-      await removeTask(db, task)
-    }
-  }, [getLocalDatabaseStatus])
+  }, [calendarUpdatesNeeded])
 
   useEffect(() => {
-    const updateLocalDatabaseStatus = async () => {
-      const isSynced = await getLocalDatabaseStatus()
-      setIsLocalDatabaseSynced(isSynced)
+    const syncChangesWithLocalDatabase = async () => {
+      const changesToUpdate = await compareCalendarData(
+        localData,
+        lastSyncedLocalData,
+      )
+
+      if (calendarUpdatesNeeded(changesToUpdate)) {
+        await updateLocalDatabase(changesToUpdate)
+        await updateLocalData()
+        await setLastSyncedLocalData(localData)
+      }
     }
 
-    if (isDatabaseSetup) {
-      updateLocalDatabaseStatus()
-    }
-  }, [getLocalDatabaseStatus])
+    syncChangesWithLocalDatabase()
+  }, [compareCalendarData])
 
   useEffect(() => {
-    const syncLocalDatabase = async () => {
-      await updateLocalDatabase()
-      await refetchLocalData()
+    const syncLocalDatabaseWithGoogleCalendar = async () => {
+      const updatedGoogleData = await fetchGoogleCalendarData()
+      const changesToUpdate = await compareCalendarData(
+        updatedGoogleData,
+        localData,
+      )
+
+      if (calendarUpdatesNeeded(changesToUpdate)) {
+        await updateLocalDatabase(changesToUpdate)
+        await updateLocalData()
+        await setLastSyncedLocalData(localData)
+      }
     }
 
-    if (isDatabaseSetup && !isLocalDatabaseSynced) {
-      syncLocalDatabase()
+    if (isConnected) {
+      syncLocalDatabaseWithGoogleCalendar()
     }
-  }, [isLocalDatabaseSynced, updateLocalDatabase, refetchLocalData])
+  }, [isConnected, compareCalendarData])
 }
