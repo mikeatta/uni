a50b8a692f0530e763d00c7b9a95d6a26b99b053
Mike Atta <saintsikorski@gmail.com>
2024-08-13 21:08:54 +0200

    Add React Native Dotenv to the babel config
    This commit updates the 'babel.config.js' to include the '@env' module.

--- Sem V/Mobile-applications-programming/calendar-app/babel.config.js
+++ Sem V/Mobile-applications-programming/calendar-app/babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        allowlist: null,
+        blocklist: null,
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
+  ],
 }
