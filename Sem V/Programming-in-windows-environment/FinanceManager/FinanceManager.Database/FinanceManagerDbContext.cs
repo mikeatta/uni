fbf1cc2eb7e75a822ea06ee235934700b673345c
Mike Atta <saintsikorski@gmail.com>
2024-12-27 15:20:20 +0100

    Initialize the main window from the code-behind
    This commit uses the DbOptions-parameter constructor to start the
    'MainViewModel' from the code-behind.

--- Sem V/Programming-in-windows-environment/FinanceManager/FinanceManager.Database/FinanceManagerDbContext.cs
+++ Sem V/Programming-in-windows-environment/FinanceManager/FinanceManager.Database/FinanceManagerDbContext.cs
@@ -1,7 +1,5 @@
-using dotenv.net;
 using FinanceManager.Database.EntityModels;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using Alert = FinanceManager.Database.EntityModels.Alert;
 using FinancialGoal = FinanceManager.Database.EntityModels.FinancialGoal;
 using Report = FinanceManager.Database.EntityModels.Report;
@@ -19,25 +17,9 @@ public class FinanceManagerDbContext : DbContext
     public DbSet<Report> Reports { get; set; }
     public DbSet<Alert> Alerts { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public FinanceManagerDbContext(DbContextOptions<FinanceManagerDbContext> options)
+        : base(options)
     {
-        // Calculate the path to the .env file relative to the base directory
-        string envPath = Path.Combine(AppContext.BaseDirectory, @"..\..\..\..\FinanceManager.Database\.env");
-
-        var databaseEnv = DotEnv.Read(new DotEnvOptions(envFilePaths: new[] { envPath }, ignoreExceptions: false));
-
-        // Build the connection string using the environment variables
-        var host = databaseEnv["HOST"];
-        int port = Int32.Parse(databaseEnv["PORT"]);
-        var database = databaseEnv["DATABASE"];
-        var username = databaseEnv["USERNAME"];
-        var password = databaseEnv["PASSWORD"];
-
-        var connectionString = $"Host={host};Port={port};Database={database};Username={username};Password={password};";
-
-        // Configure the DbContext
-        optionsBuilder.UseNpgsql(connectionString);
-        optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
--- Sem V/Programming-in-windows-environment/FinanceManager/FinanceManager/App.xaml.cs
+++ Sem V/Programming-in-windows-environment/FinanceManager/FinanceManager/App.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
+using dotenv.net;
+using FinanceManager.Database;
 using FinanceManager.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace FinanceManager;
 
@@ -10,9 +15,30 @@ public partial class App : Application
 {
     protected override void OnStartup(StartupEventArgs e)
     {
+        var optionsBuilder = new DbContextOptionsBuilder<FinanceManagerDbContext>();
+
+        // Calculate the path to the .env file relative to the base directory
+        string envPath = Path.Combine(AppContext.BaseDirectory, @"..\..\..\..\FinanceManager.Database\.env");
+
+        var databaseEnv = DotEnv.Read(new DotEnvOptions(envFilePaths: new[] { envPath }, ignoreExceptions: false));
+
+        // Build the connection string using the environment variables
+        var host = databaseEnv["HOST"];
+        int port = Int32.Parse(databaseEnv["PORT"]);
+        var database = databaseEnv["DATABASE"];
+        var username = databaseEnv["USERNAME"];
+        var password = databaseEnv["PASSWORD"];
+
+        var connectionString =
+            $"Host={host};Port={port};Database={database};Username={username};Password={password};";
+
+        // Configure the DbContext
+        optionsBuilder.UseNpgsql(connectionString);
+        optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+
         MainWindow = new MainWindow()
         {
-            DataContext = new MainViewModel()
+            DataContext = new MainViewModel(optionsBuilder.Options)
         };
 
         MainWindow.Show();
