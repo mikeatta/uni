cb317192a621f5b03e634b9434f6e5a693513b6c
Mike Atta <saintsikorski@gmail.com>
2025-01-17 15:34:27 +0100

    Add 'converters.h' to 'main.c'
    Changes 'main.c' -- imports the 'converters.h' header file.
    
    Changes 'main.c' -- removes 'is_valid_hex()' and
    'convert_char_string_to_uint16()', as the functions were moved to
    'converters.h'.
    
    This commit implements the 'converters' header file in the main program
    file and cleans up the left over functions.

--- Sem III/Microprocessor-programming/AM2320-sensor/Core/Src/main.c
+++ Sem III/Microprocessor-programming/AM2320-sensor/Core/Src/main.c
@@ -28,6 +28,7 @@
 #include "string.h"
 #include "math.h"
 #include "am2320_it.h"
+#include "converters.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -212,10 +213,9 @@ uint8_t receive_frame(uint8_t *sender_address, uint8_t *data)
 
 			for (uint8_t i = 0; i < 5; i++)
 			{
-				if ((*crc_value_ptr < '0' || *crc_value_ptr > '9') &&
-					(*crc_value_ptr < 'A' || *crc_value_ptr > 'F'))
+				if (!is_valid_hex((const char *)crc_value_ptr, 1))
 				{
-					continue; // Skip the frame if CRC character is not a number
+					continue; // Skip the frame if CRC character is not a valid hex number
 				}
 				else if (*crc_value_ptr != crc_string[i])
 				{
@@ -506,58 +506,6 @@ void AM2320_SendSensorDataFrame(uint8_t *recipient, uint16_t *read_idx, float te
 	send_frame(recipient, sensor_read_output, crc_value);
 }
 
-/**
- * @brief  Checks if the given string contains only hexadecimal values.
- *
- * @param  str A pointer to the array containing a string.
- * @param  length The length of the given string.
- * @retval Returns 1 when the string is valid, 0 otherwise.
- */
-uint8_t is_valid_hex(const char *str, uint8_t length)
-{
-	for (uint8_t i = 0; i < length; i++)
-	{
-		if ((str[i] > '9' || str[i] < '0') &&
-			(str[i] > 'F' || str[i] < 'A'))
-		{
-			return 0;
-		}
-	}
-	return 1; // All chars are valid hex digits
-}
-
-/**
- * @brief  Converts a hexadecimal string to an unsigned 16-bit integer.
- *
- * @param  string_array A pointer to the string-digit array.
- * @param  string_length An unsigned int representing the amount of characters to convert.
- * @retval A 16-bit unsigned integer.
- */
-uint16_t convert_char_string_to_uint16(uint8_t *string_array, uint8_t string_length)
-{
-	uint16_t result = 0;
-
-	for (uint8_t i = 0; i < string_length; i++)
-	{
-		uint8_t character = *string_array++;
-
-		if (isdigit(character))
-		{
-			character -= '0';
-		}
-		else if (character >= 'A' && character <= 'F')
-		{
-			character -= 'A' - 10;
-		}
-		else if (character >= 'a' && character <= 'f')
-		{
-			character -= 'a' - 10;
-		}
-
-		result = (result << 4) | character;
-	}
-
-	return result;
 }
 
 /**
