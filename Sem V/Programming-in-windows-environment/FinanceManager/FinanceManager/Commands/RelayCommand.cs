ad8b5a0e566c06cf18556d5c64da1013065ff460
Mike Atta <saintsikorski@gmail.com>
2025-01-31 13:24:32 +0100

    Update 'RelayCommand.cs'

--- Sem V/Programming-in-windows-environment/FinanceManager/FinanceManager/Commands/RelayCommand.cs
+++ Sem V/Programming-in-windows-environment/FinanceManager/FinanceManager/Commands/RelayCommand.cs
@@ -4,23 +4,13 @@ namespace FinanceManager.Commands;
 
 public class RelayCommand : ICommand
 {
-    private readonly Predicate<object> _canExecute;
     private readonly Action<object> _execute;
+    private readonly Func<object, bool> _canExecute;
 
-    public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+    public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
     {
-        _execute = execute;
-        _canExecute = canExecute;
-    }
-
-    public bool CanExecute(object parameter)
-    {
-        return _canExecute == null || _canExecute(parameter);
-    }
-
-    public void Execute(object parameter)
-    {
-        _execute(parameter);
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        _canExecute = canExecute ?? (_ => true);
     }
 
     public event EventHandler CanExecuteChanged
@@ -28,4 +18,8 @@ public class RelayCommand : ICommand
         add => CommandManager.RequerySuggested += value;
         remove => CommandManager.RequerySuggested -= value;
     }
+
+    public bool CanExecute(object parameter) => _canExecute(parameter);
+
+    public void Execute(object parameter) => _execute(parameter);
 }
